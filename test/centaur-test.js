d058d626b11973515a0571516b48cecf6f1f2e82
Lovisa Svallingson <lovisa.svallingson@ca.com>
2016-09-29 20:25:06 -0600

    Refactor and finish centaur-test

--- test/centaur-test.js
+++ test/centaur-test.js
@@ -8,12 +8,12 @@ describe('Centaur', function () {
   });
 
   it.skip('should instantiate our good friend, Blocky', function () {
-    var centaur = new Centaur();
-    assert.isObject(Centaur);
+    var centaur = new Centaur('Blocky');
+    assert.equal(centaur.name, 'Blocky');
   });
 
   it.skip('should have a name', function () {
-    var centaur = new Centaur('George', 'Palomino');
+    var centaur = new Centaur('George');
     assert.equal(centaur.name, 'George');
   });
 
@@ -23,109 +23,142 @@ describe('Centaur', function () {
   });
 
   it.skip('should have excellent bow skills', function() {
-    var centaur = new Centaur('George', 'Palomino');
+    var centaur = new Centaur('George');
     assert.equal(centaur.shoot(), 'Twang!!!');
   });
 
   it.skip('should have a horse sound when it runs', function() {
-    var centaur = new Centaur('George', 'Palomino');
+    var centaur = new Centaur('George');
     assert.equal(centaur.run(), 'Clop clop clop clop!!!');
   });
 
   it.skip('should not be cranky when first created', function() {
-    var centaur = new Centaur('George', 'Palomino');
-    assert(!centaur.cranky());
+    var centaur = new Centaur('George');
+    assert(centaur.cranky, false);
   });
 
   it.skip('should be standing up when first created', function() {
-    var centaur = new Centaur('George', 'Palomino');
+    var centaur = new Centaur('George');
     assert(centaur.standing);
   });
 
   it.skip('should be cranky after running or shooting a bow three times', function() {
-    var centaur = new Centaur('George', 'Palomino');
-    assert(!centaur.cranky);
+    var centaur = new Centaur('George');
+
+    assert(centaur.cranky, false);
+
     centaur.shoot()
     centaur.run()
     centaur.shoot()
+
     assert(centaur.cranky)
   });
 
-  function shoot(roar) {
-    for(var i = 0; i < 3; i++) {
-      roar.shoot()
-    }
-  }
-
   it.skip('should not shoot when cranky', function() {
-    var centaur = new Centaur('George', 'Palomino');
-    shoot(centaur)
+    var centaur = new Centaur('George');
+
+    for (var i = 0; i < 3; i++)  {
+      centaur.shoot();
+    }
+
     assert.equal(centaur.shoot(), 'NO!');
   });
 
   it.skip('should not sleep while standing', function() {
-    var centaur = new Centaur('George', 'Palomino');
+    var centaur = new Centaur('George');
     assert.equal(centaur.sleep(), 'NO!');
   });
 
   it.skip('should not sleep while standing', function() {
-    var centaur = new Centaur('George', 'Palomino');
+    var centaur = new Centaur('George');
     assert.equal(centaur.sleep(), 'NO!');
   });
 
   it.skip('after laying down it is not standing', function() {
-    var centaur = new Centaur('George', 'Palomino');
+    var centaur = new Centaur('George');
+
     assert(centaur.standing);
     assert(!centaur.layingDown);
+
     centaur.layDown();
+
     assert(!centaur.standing);
-    assert(centaura.layingDown);
+    assert(centaur.layingDown);
+
     centaur.standUp();
+
     assert(centaur.standing);
     assert(!centaur.layingDown);
   });
 
   it.skip('should not run or shoot while laying down', function() {
-    var centaur = new Centaur('George', 'Palomino');
+    var centaur = new Centaur('George');
+
     centaur.layDown();
+
     assert.equal(centaur.shoot(), 'NO!');
     assert.equal(centaur.run(), 'NO!');
   });
 
   it.skip('should not run or shoot while laying down', function() {
-    var centaur = new Centaur('George', 'Palomino');
+    var centaur = new Centaur('George');
+
     centaur.layDown();
+
     assert.equal(centaur.shoot(), 'NO!');
     assert.equal(centaur.run(), 'NO!');
   });
 
   it.skip('should be able to sleep when laying down', function() {
-    var centaur = new Centaur('George', 'Palomino');
+    var centaur = new Centaur('George');
     centaur.layDown();
     assert.equal(centaur.sleep(), 'ZZZZ');
   });
 
   it.skip('should not be cranky after sleeping', function() {
-    var centaur = new Centaur('George', 'Palomino');
-    shoot(centaur);
+    var centaur = new Centaur('George');
+
+    for (var i = 0; i < 3; i++)  {
+      centaur.shoot();
+    }
+
     assert(centaur.cranky);
+
     centaur.layDown();
+
     assert.equal(centaur.sleep(), 'ZZZZ');
-    assert(!centaur.cranky);
+    assert.equal(centaur.cranky(), false);
     assert.equal(centaur.shoot(),'Twang!!!');
   });
 
   it.skip('should not be cranky after drinking potion', function() {
+    var centaur = new Centaur('George');
 
+    for (var i = 0; i < 3; i++)  {
+      centaur.shoot();
+    }
+
+    centaur.drinkPotion();
+
+    assert(centaur.cranky)
   });
 
   it.skip('should only drink potion while standing', function() {
+    var centaur = new Centaur('George');
 
+    centaur.drinkPotion();
+    centaur.layDown();
+
+    assert.equal(centaur.drinkPotion(), 'Not while I\'m standing!');
   });
 
   it.skip('should be cranky if it drinks potion while rested', function() {
+    var centaur = new Centaur('George');
 
+    assert(centaur.cranky, false);
+
+    centaur.drinkPotion();
+
+    assert(centaur.cranky);
   });
-
-
 });
