ca360cf1cb4d343dd8f0af052c9762330e77d482
Robbie Jaeger <jaeger.rob@gmail.com>
2016-10-04 21:37:48 -0600

    Add semicolons for consistency

--- test/pirate-test.js
+++ test/pirate-test.js
@@ -45,19 +45,19 @@ describe('Pirate', function () {
 
   it.skip('should be able to rob ships', function() {
     var pirate = new Pirate('JeffBeard');
-    assert.equal(pirate.robShip(), 'YAARRR!')
+    assert.equal(pirate.robShip(), 'YAARRR!');
   });
 
   it.skip('should start with a booty of 0', function() {
     var pirate = new Pirate('JeffBeard');
-    assert.equal(pirate.booty, 0)
+    assert.equal(pirate.booty, 0);
   });
 
   it.skip('should have a booty of 100 after robShip()', function() {
     var pirate = new Pirate('JeffBeard');
-    assert.equal(pirate.booty, 0)
+    assert.equal(pirate.booty, 0);
 
-    pirate.robShip()
-    assert.equal(pirate.booty, 100)
+    pirate.robShip();
+    assert.equal(pirate.booty, 100);
   });
 });
