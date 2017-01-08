033ca2fdbc2cd82aad2c4c74b3169a5b340521d1
Bree Thomas <bree@turing.io>
2017-01-08 11:43:54 -0700

    Fix typo: 'were' to 'wear'

--- objects-and-functions/test/trick-or-treater-test.js
+++ objects-and-functions/test/trick-or-treater-test.js
@@ -10,13 +10,13 @@ describe('TrickOrTreater', function () {
     assert.isFunction(TrickOrTreater);
   });
 
-  it.skip('should were a costume', function () {
+  it.skip('should wear a costume', function () {
     costume = new Costume('Cowboy');
     trickOrTreater = new TrickOrTreater(costume);
     assert.equal(trickOrTreater.dressedUpAs, 'Cowboy');
   });
 
-  it.skip('should were a different costume', function () {
+  it.skip('should wear a different costume', function () {
     costume = new Costume('Cowboy');
     trickOrTreater = new TrickOrTreater(costume);
     assert.equal(trickOrTreater.dressedUpAs, 'Cowboy');
