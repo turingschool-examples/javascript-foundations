0881638b305827385239d0ebdb21f1a85e9ada9d
Eduardo <jeduardorjx@gmail.com>
2019-07-06 13:47:17 -0600

    Complete Unicorn exercise and test

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,6 +1,25 @@
 class Unicorn {
-  constructor() {
+  constructor(name, color = 'white') {
+    this.name = name;
+    this.color = color
+  }
+
+  isWhite() {
+    console.log(this)
+    if (this.color !== 'white') {
+      return false;
+    }
+  }
+
+  says(message) {
+    return `**;* ${message} *;**`;
   }
 }
 
 module.exports = Unicorn;
+
+//Process to follow...
+//1. Read the test
+//2. Ask yourself if it's going to fail and why
+//3. Run the test
+//4. Make it pass
--- mythical-creatures/test/unicorn-test.js
+++ mythical-creatures/test/unicorn-test.js
@@ -3,41 +3,41 @@ var Unicorn = require('../exercises/unicorn');
 
 describe('Unicorn', function() {
 
-  it.skip('should be a function', function() {
+  it('should be a function', function() {
     assert.isFunction(Unicorn);
   });
 
-  it.skip('should instantiate our good friend, Unicorn', function() {
+  it('should instantiate our good friend, Unicorn', function() {
     var unicorn = new Unicorn();
     assert.isObject(unicorn);
   });
 
-  it.skip('should have a name', function() {
+  it('should have a name', function() {
     var unicorn = new Unicorn('Bree');
     assert.equal(unicorn.name, 'Bree');
   });
 
-  it.skip('should be able to have a different name', function() {
+  it('should be able to have a different name', function() {
     var unicorn = new Unicorn('Pam');
     assert.equal(unicorn.name, 'Pam');
   });
 
-  it.skip('should have a color', function() {
+  it('should have a color', function() {
     var unicorn = new Unicorn('TayTay', 'blue');
     assert.equal(unicorn.color, 'blue');
   });
 
-  it.skip('should be white as default', function() {
+  it('should be white as default', function() {
     var unicorn = new Unicorn('Louisa');
     assert.equal(unicorn.color, 'white');
   });
 
-  it.skip('should refute isWhite', function() {
+  it('should refute isWhite', function() {
     var unicorn = new Unicorn('Steve', 'red');
     assert.equal(unicorn.isWhite(), false);
   });
 
-  it.skip('should say sparklying stuff', function() {
+  it('should say sparklying stuff', function() {
     var unicorn = new Unicorn('Brenna');
     assert.equal(unicorn.says('Wonderful!'), '**;* Wonderful! *;**');
     assert.equal(unicorn.says('Batman has a great smile'), '**;* Batman has a great smile *;**');
