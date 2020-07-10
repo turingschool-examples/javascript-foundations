2aa3bb80537ddc109bbac85e09c8247891ad73f6
Michael Walker <Michael.e.walker.87@gmail.com>
2020-07-09 22:20:39 -0600

    Added work to  Unicorn

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -2,7 +2,24 @@ class Unicorn {
   constructor(name, color) {
     this.name = name;
     this.color = color;
-  }
-}
+
+    if (color === undefined) {
+      this.color = 'white';
+    } else {
+      this.color = color;
+    };
+  };
+
+  isWhite() {
+    if (this.color !== 'white') {
+      return false;
+    };
+  };
+
+  says(message) {
+    return `**;* ${message} *;**`;
+  };
+};
+
 
 module.exports = Unicorn;
