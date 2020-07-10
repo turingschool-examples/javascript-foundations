623d354263743ede92ff30820d6bcb291e0e7e68
Michael Walker <Michael.e.walker.87@gmail.com>
2020-07-09 22:36:44 -0600

    Added and completed vampire files

---
+++ mythical-creatures/exercises/vampire.js
@@ -0,0 +1,18 @@
+class Vampire {
+  constructor(name, pet, thirsty) {
+    this.name = name;
+    this.thirsty = true;
+
+    if (pet === undefined) {
+      this.pet = 'bat';
+    } else {
+      this.pet = pet;
+    };
+  };
+
+    drink() {
+      this.thirsty = false;
+    };
+};
+
+module.exports = Vampire;
