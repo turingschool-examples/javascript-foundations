c30fe8045cc34d63e7f661daa344d3c02063e921
Joe Lopez <lopez.4.joseph@gmail.com>
2020-08-29 18:08:15 -0600

    Make unicorn pass tests

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,23 @@
 class Unicorn {
   constructor(name, color) {
     this.name = name;
-    this.color = color;
+    this.color = color || "white";
+  }
+
+   isWhite() {
+    if (this.color !== 'white') {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  says(phrase) {
+    return `**;* ${phrase} *;**`
   }
 }
 
+
 module.exports = Unicorn;
+
+
--- mythical-creatures/test/unicorn-test.js
+++ mythical-creatures/test/unicorn-test.js
@@ -32,19 +32,19 @@ describe('Unicorn', function() {
     assert.equal(unicorn.color, 'blue');
   });
 
-  it.skip('should be white as default', function() {
+  it('should be white as default', function() {
     var unicorn = new Unicorn('Louisa');
 
     assert.equal(unicorn.color, 'white');
   });
 
-  it.skip('should refute isWhite', function() {
+  it('should refute isWhite', function() {
     var unicorn = new Unicorn('Steve', 'red');
 
     assert.equal(unicorn.isWhite(), false);
   });
 
-  it.skip('should say sparklying stuff', function() {
+  it('should say sparklying stuff', function() {
     var unicorn = new Unicorn('Brenna');
     
     assert.equal(unicorn.says('Wonderful!'), '**;* Wonderful! *;**');
