6a89c97b6f09109cb50b6d8338a4083f0aa23b98
mainlyetcetera <ericcampbell294@gmail.com>
2020-10-13 22:15:54 -0600

    Pass isWhite refutation test

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -2,6 +2,10 @@ class Unicorn {
   constructor(name, color) {
     this.name = name;
     color === undefined ? this.color = 'white' : this.color = color;
+  }
+
+  isWhite() {
+   return this.color !== 'white' ? false : true;
   }  
   
 }
