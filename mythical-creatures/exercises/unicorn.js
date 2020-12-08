6e5544e570983570018892958f3e3f961d7657b6
Jessica Justice <jessica@jessicas-MacBook-Pro.local>
2020-12-07 19:38:37 -0700

    Complete Unicorn tests

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,7 +1,19 @@
 class Unicorn {
-  constructor(name, color) {
+  constructor(name, color= 'white') {
     this.name = name;
     this.color = color;
+  };
+
+  isWhite() {
+    if (this.color == 'white') {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  says(msgInput) {
+    return `**;* ${msgInput} *;**`;
   }
 }
 
--- mythical-creatures/test/unicorn-test.js
+++ mythical-creatures/test/unicorn-test.js
@@ -27,25 +27,25 @@ describe('Unicorn', function() {
 
   it('should have a color', function() {
     var unicorn = new Unicorn('TayTay', 'blue');
-    
+
     assert.equal(unicorn.color, 'blue');
   });
 
-  it.skip('should be white as default', function() {
+  it('should be white as default', function() {
     var unicorn = new Unicorn('Louisa');
 
     assert.equal(unicorn.color, 'white');
   });
 
-  it.skip('should refute isWhite', function() {
+  it('should refute isWhite', function() {
     var unicorn = new Unicorn('Steve', 'red');
 
     assert.equal(unicorn.isWhite(), false);
   });
 
-  it.skip('should say sparklying stuff', function() {
+  it('should say sparklying stuff', function() {
     var unicorn = new Unicorn('Brenna');
-    
+
     assert.equal(unicorn.says('Wonderful!'), '**;* Wonderful! *;**');
     assert.equal(unicorn.says('Batman has a great smile'), '**;* Batman has a great smile *;**');
   });
