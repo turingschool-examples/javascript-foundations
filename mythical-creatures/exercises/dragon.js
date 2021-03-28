f1ac17101bed909826650d2d59526b8d2f1e5a13
Taylor Galloway <gallowaytaylor@outlook.com>
2021-03-28 14:52:58 -0500

    Add Dragon

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,21 @@
+class Dragon {
+  constructor(name, rider) {
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+    this.numTimesEat = 0;
+  };
 
+  greet() {
+    return `Hi, ${this.rider}!`
+  }
+
+  eat() {
+    this.numTimesEat ++;
+    if (this.numTimesEat === 3) {
+      this.hungry = false
+    };
+  };
+};
+
+module.exports = Dragon;
