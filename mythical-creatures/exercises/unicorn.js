78771da01c2bf03717b33f75a0118166d6c6cc71
Taylor Galloway <gallowaytaylor@outlook.com>
2021-03-28 14:36:27 -0500

    Add unicorn

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,22 @@
 class Unicorn {
   constructor(name, color) {
     this.name = name;
-    this.color = color;
-  }
-}
+    if (!color) {
+      this.color = "white";
+    } else {
+      this.color = color;
+    };
+  };
+
+  isWhite() {
+    if (this.color !== "white") {
+      return false;
+    };
+  };
+
+  says(message) {
+    return `**;* ${message} *;**` 
+  };
+};
 
 module.exports = Unicorn;
