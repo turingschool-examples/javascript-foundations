7e752fb62ed83b3eb8c90a834665bb154990ddf2
Hayley Witherell <hayleywitherell@gmail.com>
2021-03-31 12:17:11 -0600

    Complete

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,21 @@
 class Unicorn {
   constructor(name, color) {
     this.name = name;
-    this.color = color;
+    if (color === undefined) {
+      this.color = "white";
+    } else {
+      this.color = color;
+    }
   }
-}
-
+    isWhite() {
+      if (this.color !== "white") {
+        return false;
+      }
+    }
+    says(message) {
+      return `**;* ${message} *;**`;
+    }
+  }
 module.exports = Unicorn;
+
+// npm test mythical-creatures/test/unicorn-test.js
