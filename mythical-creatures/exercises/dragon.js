c7a7d743e622672966884e2971f1bb83f687093a
Hayley Witherell <hayleywitherell@gmail.com>
2021-03-31 12:41:54 -0600

    Complete

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,26 @@
 
+class Dragon {
+  constructor(name, rider){
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+    this.timesEaten = 0;
+  }
+
+  greet() {
+    return "Hi, " + this.rider + "!"
+  }
+
+  eat() {
+  this.timesEaten++;
+    if (this.timesEaten >= 3) {
+      this.hungry = false;
+  }
+}
+}
+
+module.exports = Dragon
+
+
+
+// npm test mythical-creatures/test/dragon-test.js
