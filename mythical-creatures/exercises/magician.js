fcf92fb2e0486860578ad38002128973b173b6c3
Hayley Witherell <hayleywitherell@gmail.com>
2021-04-06 12:56:05 -0600

    Start magician with Taryn

---
+++ mythical-creatures/exercises/magician.js
@@ -0,0 +1,21 @@
+class Magician {
+  constructor(magicianMan) {
+    this.name = "The Great " + magicianMan.name;
+    this.assistant = magicianMan.assistant;
+    this.favoriteAccessory = magicianMan.clothing;
+    if (magicianMan.name === "Scott") {
+      this.favoriteAccessory = "top hat";
+    }
+    this.favoriteAccessory = magicianMan.clothing || 'top hat';
+  }
+  performIncantation(spell) {
+    // return words
+    return spell.toUpperCase() + "!"
+    // make words capital
+    // add !
+  }
+}
+
+module.exports = Magician
+
+//npm test mythical-creatures/test/magician-test.js
