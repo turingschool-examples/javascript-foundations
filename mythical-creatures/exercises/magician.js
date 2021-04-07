a88137fa34f3c516ef023d1cb49c06e9b7afa769
Hayley Witherell <hayleywitherell@gmail.com>
2021-04-07 12:31:40 -0600

    Finalize magician

--- mythical-creatures/exercises/magician.js
+++ mythical-creatures/exercises/magician.js
@@ -2,17 +2,28 @@ class Magician {
   constructor(magicianMan) {
     this.name = "The Great " + magicianMan.name;
     this.assistant = magicianMan.assistant;
-    this.favoriteAccessory = magicianMan.clothing;
-    if (magicianMan.name === "Scott") {
-      this.favoriteAccessory = "top hat";
-    }
     this.favoriteAccessory = magicianMan.clothing || 'top hat';
+    this.confidencePercentage = 10;
   }
+  
   performIncantation(spell) {
-    // return words
     return spell.toUpperCase() + "!"
-    // make words capital
-    // add !
+  }
+
+  performTrick() {
+    this.confidencePercentage += 10;
+    if (this.favoriteAccessory === "top hat") {
+      return "PULL RABBIT FROM TOP HAT";
+    } else {
+      return "PULL DOVE FROM SLEEVE";
+    }
+  }
+  performShowStopper() {
+    if (this.confidencePercentage >= 100 && this.assistant === true) {
+      return "WOW! The magician totally just sawed that person in half!"
+    } else {
+      return "Oh no, this trick is not ready!"
+    }
   }
 }
 
