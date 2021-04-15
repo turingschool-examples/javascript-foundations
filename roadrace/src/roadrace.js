7a4c9b7d8280a9535a95abcf03b09ae102f73729
Heather Faerber <heather@turing.com>
2021-04-15 11:53:09 -0600

    Remove solved implementation code

--- roadrace/src/roadrace.js
+++ roadrace/src/roadrace.js
@@ -1,26 +1,5 @@
 class Roadrace {
-  constructor(theRace) {
-    this.name = theRace.title;
-    this.location = theRace.city;
-    this.distance = null;
-    this.participants = [];
-  }
 
-  setDistance(miles) {
-    this.distance = miles;
-    return `The ${this.name} in ${this.location} is a ${this.distance} mile race.`
-  }
-
-  registerParticipants(runner) {
-    this.participants.push(runner);
-  }
-
-  completeRace() {
-    for (var i = 0; i < this.participants.length; i++) {
-      // this.participants[i].completedRaces.push(this.name);
-      this.participants[i].runRace(this.name, this.distance);
-    }
-  }
 }
 
 module.exports = Roadrace;
