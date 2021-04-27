6efaca18476b50beddd197976f11926e1536f1e7
Gabrielle Joyce <68332132+gaj23@users.noreply.github.com>
2021-04-27 15:15:54 -0600

    Pass all existing tests

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,7 +1,15 @@
 class Unicorn {
   constructor(name, color) {
     this.name = name;
-    this.color = color;
+    this.color = color || 'white';
+  }
+
+  isWhite = () => {
+    return this.color === 'white' || this.color === 'White' ? true : false
+  }
+
+  says = (words) => {
+    return `**;* ${words} *;**`
   }
 }
 
