e0a49c53506aeca25fd9249d51dafbce97962c22
Gabrielle Joyce <68332132+gaj23@users.noreply.github.com>
2021-04-28 13:38:39 -0600

    Passed all existing tests

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,24 @@
+class Dragon {
+  constructor(name, rider){
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+    this.eatingCount = 0;
+    this.sick = false;
+  }
 
+  greet = () => {
+    return `Hi, ${this.rider}!`
+  }
+
+  eat = () => {
+    this.eatingCount += 1;
+
+    if(this.eatingCount > 2) {
+      this.hungry = false;
+    }
+  }
+}
+
+
+module.exports = Dragon;
