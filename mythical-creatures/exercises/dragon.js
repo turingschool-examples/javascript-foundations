17b5edf6d9df73879ed88e9f4183fdaf5db3ba22
Phil Lewis <phillewis1985@gmail.com>
2021-10-18 17:48:47 -0600

    Pass all but last dragon test

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,15 @@
+class Dragon {
+  constructor(name, rider) {
+    this.name = name;
+    this.rider = rider;
+    this.greet = function() {
+      return `Hi, ${this.rider}!`;
+    }
+    this.hungry = true;
+    this.eat = function() {
+      var counter = 0;
+    }
+  }
+}
 
+module.exports = Dragon
