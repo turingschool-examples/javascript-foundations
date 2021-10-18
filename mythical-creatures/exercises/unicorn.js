b8fe32a40ea7892bb81fe2fb323de9126da60d71
Phil Lewis <phillewis1985@gmail.com>
2021-10-18 17:13:06 -0600

    Pass all unicorn tests

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,7 +1,16 @@
 class Unicorn {
-  constructor(name, color) {
+  constructor(name, color = 'white') {
     this.name = name;
     this.color = color;
+    this.isWhite = function(color) {
+      if (color !== 'white') {
+        return false;
+      }
+    return true;
+    };
+    this.says = function(phrase) {
+      return `**;* ${phrase} *;**`;
+    }
   }
 }
 
