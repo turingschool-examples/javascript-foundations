a933c0b87ff7b41d6d7159a142a98f3bb3119c14
Phil Lewis <phillewis1985@gmail.com>
2021-10-19 08:25:10 -0600

    Pass all magician tests

--- mythical-creatures/test/magician-test.js
+++ mythical-creatures/test/magician-test.js
@@ -39,7 +39,7 @@ describe('Magician', function() {
     assert.equal(magician.favoriteAccessory, 'cape')
   });
 
-  it.skip('should be able to perform an incantation', function() {
+  it('should be able to perform an incantation', function() {
     var magician = new Magician({ name: 'Scott' });
 
     var spell = magician.performIncantation('abracadabra');
@@ -49,7 +49,7 @@ describe('Magician', function() {
     assert.equal(charm, 'ALLAKAZAAM!');
   });
 
-  it.skip('should be able to pull a rabbit from the hat, if the magician has a top hat', function() {
+  it('should be able to pull a rabbit from the hat, if the magician has a top hat', function() {
     var magician1 = new Magician({ name: 'Hannah' });
     var magician2 = new Magician({ name: 'Kayla', clothing: 'top hat' });
 
@@ -60,7 +60,7 @@ describe('Magician', function() {
     assert.equal(spell2, 'PULL RABBIT FROM TOP HAT');
   });
 
-  it.skip('should be able to pull a dove from the sleeve, if the magician does not have a top hat', function() {
+  it('should be able to pull a dove from the sleeve, if the magician does not have a top hat', function() {
     var magician = new Magician({ name: 'Leta', clothing: 'cape' });
 
     var spell = magician.performTrick();
@@ -68,13 +68,13 @@ describe('Magician', function() {
     assert.equal(spell, 'PULL DOVE FROM SLEEVE');
   });
 
-  it.skip('should start not very confident', function() {
+  it('should start not very confident', function() {
     var magician = new Magician({ name: 'Leta' });
 
     assert.equal(magician.confidencePercentage, 10);
   });
 
-  it.skip('should gain confidence after performing spells', function() {
+  it('should gain confidence after performing spells', function() {
     var magician = new Magician({ name: 'Leta' });
 
     magician.performTrick();
@@ -88,7 +88,7 @@ describe('Magician', function() {
     assert.equal(magician.confidencePercentage, 50);
   });
 
-  it.skip('should not be able to saw someone in half unless they\'re 100% confident', function() {
+  it('should not be able to saw someone in half unless they\'re 100% confident', function() {
     var magician = new Magician({ name: 'Scott', assistant: true });
 
     assert.equal(magician.performShowStopper(), 'Oh no, this trick is not ready!');
@@ -100,7 +100,7 @@ describe('Magician', function() {
     assert.equal(magician.performShowStopper(), 'WOW! The magician totally just sawed that person in half!');
   });
 
-  it.skip('should not be able to saw someone in half unless they have an assistant', function () {
+  it('should not be able to saw someone in half unless they have an assistant', function () {
     var magician1 = new Magician({ name: 'Scott', assistant: false });
     var magician2 = new Magician({ name: 'Hannah', assistant: true });
 
