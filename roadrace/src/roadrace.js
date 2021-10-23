1066f1740f76fd903c48c265dc53ed1337ab4190
Phil Lewis <phillewis1985@gmail.com>
2021-10-23 11:36:24 -0600

    Pass all roadrace tests

--- roadrace/src/roadrace.js
+++ roadrace/src/roadrace.js
@@ -1,5 +1,22 @@
 class Roadrace {
-
+  constructor(race) {
+    this.name = race.title;
+    this.location = race.city;
+    this.distance = 0;
+    this.participants = [];
+  }
+  setDistance(miles) {
+    this.distance = miles;
+    return `The ${this.name} in ${this.location} is a ${this.distance} mile race.`
+  }
+  registerParticipants(runner) {
+    this.participants.push(runner);
+  }
+  completeRace() {
+    for (let i = 0; i < this.participants.length; i++) {
+      this.participants[i].runRace(this.name, this.distance);
+    }
+  }
 }
 
 module.exports = Roadrace;
