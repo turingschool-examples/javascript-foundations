72a51c4804b7f68f69db0986423cea8c5f61f004
Phil Lewis <phillewis1985@gmail.com>
2021-10-23 11:48:17 -0600

    Pass all material tests

--- crafting/src/material.js
+++ crafting/src/material.js
@@ -1,5 +1,20 @@
 class Material {
-
+  constructor(name, price, amount, units) {
+    this.name = name;
+    this.price = price;
+    this.amount = amount;
+    this.units = units;
+  }
+  useMaterial(amountUsed) {
+    if (amountUsed > this.amount) {
+      return `You don\'t have enough ${this.name}! Try using ${this.amount} ${this.units} or less.`;
+    }
+    this.amount -= amountUsed;
+    return `You now have ${this.amount} ${this.units} of ${this.name} left.`;
+  }
+  calculateMaterialCost() {
+    return this.amount * this.price;
+  }
 }
 
 module.exports = Material;
