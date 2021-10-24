c8fde400caaa930b77681e454792952bf98cfd98
Phil Lewis <phillewis1985@gmail.com>
2021-10-24 08:41:46 -0600

    Pass all costume tests

--- trick-or-treat/test/costume-test.js
+++ trick-or-treat/test/costume-test.js
@@ -3,17 +3,17 @@ var Costume = require('../lib/costume')
 
 describe('Costume', function () {
 
-  it.skip('should be a function', function () {
+  it('should be a function', function () {
     assert.isFunction(Costume);
   });
 
-  it.skip('should have a style', function () {
+  it('should have a style', function () {
     var costume = new Costume('dinosaur');
 
     assert.equal(costume.style, 'dinosaur');
   });
 
-  it.skip('should have another style of costume', function() {
+  it('should have another style of costume', function() {
     var costume = new Costume('ghost');
 
     assert.equal(costume.style, 'ghost');
