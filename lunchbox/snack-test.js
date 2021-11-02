ecc67f9392f454522e7d260e06c14b89d9bf46c7
Phil Lewis <phillewis1985@gmail.com>
2021-11-02 16:59:39 -0600

    Pass all snack tests

--- lunchbox/snack-test.js
+++ lunchbox/snack-test.js
@@ -3,13 +3,13 @@ var Snack = require('./snack');
 
 describe('Snack', function() {
 
-  it.skip('should be extra delicious', function() {
+  it('should be extra delicious', function() {
     var pistachio = new Snack("Pistachio");
 
     assert.equal(pistachio.deliciousLevel, "extra");
   });
 
-  it.skip('should have a type', function() {
+  it('should have a type', function() {
     var pistachio = new Snack("Pistachio");
     var fruitSnack = new Snack("Fruit Snack");
 
@@ -17,19 +17,19 @@ describe('Snack', function() {
     assert.equal(fruitSnack.type, "Fruit Snack");
   });
 
-  it.skip('should start fully stocked', function() {
+  it('should start fully stocked', function() {
     var pistachio = new Snack("Pistachio");
 
     assert.equal(pistachio.amount, 100);
   });
 
-  it.skip('should start NOT inside of a lunch box', function() {
+  it('should start NOT inside of a lunch box', function() {
     var pistachio = new Snack("Pistachio");
 
     assert.equal(pistachio.isInLunchBox, false);
   });
 
-  it.skip('should see stock decrease after eaten', function() {
+  it('should see stock decrease after eaten', function() {
     var pistachio = new Snack("Pistachio");
 
     pistachio.getEaten();
@@ -38,7 +38,7 @@ describe('Snack', function() {
     assert.equal(pistachio.amount, 70);
   });
 
-  it.skip('should be cutting it close if 20% or less remaining', function() {
+  it('should be cutting it close if 20% or less remaining', function() {
     var pistachio = new Snack("Pistachio");
 
     pistachio.getEaten();
@@ -53,7 +53,7 @@ describe('Snack', function() {
     assert.equal(pistachio.cuttingItClose, true);
   });
 
-  it.skip('should be considered healthy if the word "fruit" is in it', function() {
+  it('should be considered healthy if the word "fruit" is in it', function() {
     var pistachio = new Snack("Pistachio");
     var mixedFruit = new Snack("Mixed fruit");
     var fruit = new Snack("Fruit");
