e483cfc89dcd162790fd3f3d9e7b274783046610
Matthew Meier <mmeier211@gmail>
2022-02-10 22:09:25 -0600

    Unicorn finally done

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,18 @@
 class Unicorn {
   constructor(name, color) {
     this.name = name;
-    this.color = color;
+    this.color = color
+    if (color === undefined){
+      this.color = 'white'
+    };
+  }
+  isWhite() {
+    if (this.color === 'white') {
+      return true;
+    } else return false;
+  }
+  says(sentence) {
+    return `**;* ${sentence} *;**`
   }
 }
-
 module.exports = Unicorn;
