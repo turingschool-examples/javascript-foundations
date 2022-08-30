102c9318bcbbef3ade5ecb5ac492570930c34884
Eleanor Ruth <ellie.ruth@me.com>
2022-08-29 18:23:02 -0600

    feat add unicorn class

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,7 +1,21 @@
 class Unicorn {
   constructor(name, color) {
     this.name = name;
-    this.color = color;
+    if (color === undefined) {
+      this.color = 'white';
+    } else {
+      this.color = color;
+    }
+  }
+  isWhite() {
+    if (this.color === 'white') {
+      return true
+    } else {
+      return false
+    }
+  }
+  says(words) {
+    return '**;* ' + words + ' *;**'
   }
 }
 
