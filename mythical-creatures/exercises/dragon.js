d8fa3b4a187ccf0604de38ce722af198b1f94959
Patt Sookmark <pijiya@sookmark.com>
2022-10-18 00:04:50 -0600

    Add class

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,3 +1,44 @@
-function dragon (){
-    return
+
+class  Dragon {
+    constructor (name,rider){
+    this.name= name
+    this.rider=rider
+    this.eatting = 0
+    this.hungry = true
+    }
+
+    greet(){
+      return  `Hi, ${this.rider}!`
+    }
+    
+    eat(){
+        this.eatting = this.eatting + 1
+
+        if(this.eatting >= 3){
+            this.hungry = false
+       } else{
+            this.hungry = true
+       }
+        return this.eatting
+    }
+    check(){
+        if(this.eatting >= 3){
+             this.hungry = false
+        } else{
+             this.hungry = true
+        }
+        return this.hungry
+    }
+    
 }
+
+var dragon = new Dragon('Lady Vox');
+dragon.eat();
+dragon.eat();
+dragon.eat();
+console.log('should be 3', dragon.eatting)
+console.log(dragon.check())
+
+module.exports = 
+    Dragon
+ 
--- mythical-creatures/test/dragon-test.js
+++ mythical-creatures/test/dragon-test.js
@@ -3,17 +3,17 @@ var Dragon = require('../exercises/dragon');
 
 describe('Dragon', function() {
 
-  it.skip('should be a function', function() {
+  it('should be a function', function() {
     assert.isFunction(Dragon);
   });
 
-  it.skip('should instantiate our good friend, Dragon', function() {
+  it('should instantiate our good friend, Dragon', function() {
     var smaug = new Dragon();
 
     assert.instanceOf(smaug, Dragon);
   });
 
-  it.skip('should have name', function() {
+  it('should have name', function() {
     var dragon1 = new Dragon('Jeff');
     var dragon2 = new Dragon('Louisa');
 
@@ -21,14 +21,14 @@ describe('Dragon', function() {
     assert.equal(dragon2.name, 'Louisa');
   });
 
-  it.skip('should have a rider', function() {
+  it('should have a rider', function() {
     var dragon = new Dragon('Saphira', 'Eragon');
 
     assert.equal(dragon.name, 'Saphira');
     assert.equal(dragon.rider, 'Eragon');
   });
 
-  it.skip('should greet their rider', function() {
+  it('should greet their rider', function() {
     var dragon1 = new Dragon('Gray', 'Marley');
     var dragon2 = new Dragon('Sky', 'Susie');
 
@@ -39,13 +39,13 @@ describe('Dragon', function() {
     assert.equal(greeting2, 'Hi, Susie!');
   });
 
-  it.skip('should start off being hungry', function() {
+  it('should start off being hungry', function() {
     var dragon = new Dragon('Josh');
 
     assert.equal(dragon.hungry, true);
   });
 
-  it.skip('should be full after eating 3 times', function() {
+  it('should be full after eating 3 times', function() {
     var dragon = new Dragon('Lady Vox');
 
     dragon.eat();
