b04d487e30643632c290e04246a2d35fe6e1e8be
Patt Sookmark <pijiya@sookmark.com>
2022-11-04 13:43:38 -0600

    Add function to craft

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,21 @@
 class Craft {
-
+    constructor(craftObj){
+        this.name = craftObj.type
+        this.materials = craftObj.materials
+        this.completed = false
+    }
+    completeCraft(){
+        this.completed = true
+        return 'All done! It looks great!'
+    }
+    calculateProjectTotal(){
+        var a = 0
+       for (var i = 0; i< this.materials.length; i++){
+        a+=this.materials[i].calculateMaterialCost()
+       }
+       return a
+    
+    }
 }
 
 module.exports = Craft;
--- crafting/src/material.js
+++ crafting/src/material.js
@@ -1,4 +1,20 @@
 class Material {
+    constructor(name,price,amount,units){
+        this.name = name
+        this.price = price
+        this.amount = amount
+        this.units = units
+    }
+    useMaterial(quantity){
+        if(this.amount < quantity){
+            return `You don\'t have enough ${this.name}! Try using ${this.amount} ${this.units} or less.`
+        }
+        this.amount -= quantity
+        return `You now have ${this.amount} ${this.units} of ${this.name} left.`
+    }
+    calculateMaterialCost(){
+       return this.amount*this.price
+    }
 
 }
 
