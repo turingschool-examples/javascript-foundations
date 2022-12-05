d46e06ee56c6cc47930c3e01e4570026ab28374f
Jason Kirchman <kirchman.jason@icloud.com>
2022-12-04 21:16:07 -0600

    Complete unicorn

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,20 @@
 class Unicorn {
-  constructor(name, color) {
+  constructor(name, color = 'white') {
     this.name = name;
     this.color = color;
   }
+
+  isWhite(){
+    if(this.color === 'white') {
+      return true
+    }
+    return false
+  }
+
+  says(phrase) {
+    return `**;* ${phrase} *;**`
+  }
+
 }
 
 module.exports = Unicorn;
