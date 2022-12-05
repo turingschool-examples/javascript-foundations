09a5a7ebb2c819f378c6e1f87e17c951cca69b14
Brianne Bourassa <shinybri1011@gmail.com>
2022-12-05 14:05:46 -0700

    feat: complete functionality of unicorn

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,7 +1,15 @@
 class Unicorn {
   constructor(name, color) {
     this.name = name;
-    this.color = color;
+    this.color = color || 'white';
+  }
+  isWhite(){
+    if(this.color === this.color){
+      return false
+    }
+  }
+  says(message){
+    return `**;* ${message} *;**`
   }
 }
 
