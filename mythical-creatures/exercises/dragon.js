a720a23fbb0beb9ced0d365a7730000674198140
Brianne Bourassa <shinybri1011@gmail.com>
2022-12-05 14:44:25 -0700

    feat: complete functionality of dragon

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,19 @@
+class Dragon {
+    constructor(name, rider){
+        this.name = name;
+        this.rider = rider;
+        this.hungry = true;
+        this.meals = 0
+    }
+    greet(rider){
+        return `Hi, ${this.rider}!`
+    }
+    eat(){
+        this.meals++
+        if (this.meals > 2){
+            return this.hungry = false
+    }
+}
+}
 
+module.exports = Dragon
