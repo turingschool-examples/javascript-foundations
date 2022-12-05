fb1b1456b3869e2a555799ecac34a5ae99deb052
Jason Kirchman <kirchman.jason@icloud.com>
2022-12-04 21:34:16 -0600

    Complete dragon tests

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,21 @@
+class Dragon{
+    constructor(name,rider) {
+        this.name = name;
+        this.rider = rider;
+        this.hungry = true;
+        this.belly = 0;
+    }
 
+    greet(){
+        return `Hi, ${this.rider}!`
+    }
+
+    eat(){
+        this.belly++;
+        if(this.belly >= 3){
+            this.hungry = false;
+        }
+    }
+}
+
+module.exports = Dragon;
