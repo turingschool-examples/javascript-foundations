cd824e1def6c69893f561447526786a51c9c8a2b
Christopher Baum <christopherabaum@gmail.com>
2022-12-05 22:02:21 -0600

    Complete dragon

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -3,9 +3,21 @@ class Dragon {
     constructor (name, rider) {
         this.name = name
         this.rider = rider
+        this.fed = 0
+        this.hungry = true
     }
     greet() {
         return `Hi, ${this.rider}!`
+    }
+    eat() {
+
+        this.fed++
+        console.log(this.fed)
+        if (this.fed < 3) {
+            this.hungry = true
+        } else {
+            this.hungry = false
+        }
     }
 }
 
