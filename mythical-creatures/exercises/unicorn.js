cf763589d4be273f52fe648b29c400ab9b129b9a
Kass Daniello <zertroz@gmail.com>
2022-12-05 20:29:20 -0500

    Complete unicorn

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,7 +1,19 @@
 class Unicorn {
   constructor(name, color) {
     this.name = name;
-    this.color = color;
+    if (color === undefined) {
+      this.color = "white";
+    } else {
+      this.color = color;
+    }
+  };
+  isWhite() {
+    if (this.color !== "white") {
+      return false
+    }
+  }
+  says(phrase) {
+    return `**;* ${phrase} *;**`
   }
 }
 
