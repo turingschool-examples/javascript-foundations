aba87b2deaa7fa3e7cf48e6ed9711636905647f2
Christopher Baum <christopherabaum@gmail.com>
2022-12-05 21:04:58 -0600

    Add isWhite method to unicorn

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,7 +1,16 @@
 class Unicorn {
   constructor(name, color) {
-    this.name = name;
-    this.color = color;
+    this.name = name
+    if (color === undefined) {
+      this.color = 'white'
+    } else {
+      this.color = color
+    }
+  }
+  isWhite() {
+    if (this.color !== 'white') {
+      return false
+    }
   }
 }
 
