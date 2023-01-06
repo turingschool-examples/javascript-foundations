0c71374b6e72a92926d21e9c17683764d3a482f2
Kayla Wood <kaylaewood@gmail.com>
2023-01-06 15:26:45 -0500

    Update magician tests for clarity

--- mythical-creatures/test/magician-test.js
+++ mythical-creatures/test/magician-test.js
@@ -4,12 +4,12 @@ var Magician = require('../exercises/magician');
 describe('Magician', function() {
 // Oh no! It looks like there's no text in the `it` blocks! I guess you'll have to read the tests SUPER carefully to know what the test is expecting!
 
-it.skip('', function() {
+it('', function() {
     assert.isFunction(Magician);
     assert.instanceOf(new Magician({}), Magician);
   });
 
-it.skip('', function() {
+it('', function() {
     var magician1 = new Magician({ name: 'Scott' });
     var magician2 = new Magician({ name: 'Casey' });
 
@@ -17,7 +17,7 @@ it.skip('', function() {
     assert.equal(magician2.name, 'The Great Casey');
   });
 
-it.skip('', function() {
+it('', function() {
     var magician1 = new Magician({ name: 'Scott', assistant: true });
     var magician2 = new Magician({ name: 'Casey', assistant: false });
 
@@ -28,19 +28,19 @@ it.skip('', function() {
     assert.equal(magician2.assistant, false);
   });
 
-it.skip('', function() {
+it('', function() {
     var magician = new Magician({ name: 'Scott' });
 
     assert.equal(magician.favoriteAccessory, 'top hat');
   });
 
-it.skip('', function() {
+it('', function() {
     var magician = new Magician({ name: 'Casey', clothing: 'cape' });
 
     assert.equal(magician.favoriteAccessory, 'cape')
   });
 
-it.skip('', function() {
+it('', function() {
     var magician = new Magician({ name: 'Scott' });
 
     var spell = magician.performIncantation('abracadabra');
@@ -50,7 +50,7 @@ it.skip('', function() {
     assert.equal(charm, 'ALLAKAZAAM!');
   });
 
-it.skip('', function() {
+it('', function() {
     var magician1 = new Magician({ name: 'Hannah' });
     var magician2 = new Magician({ name: 'Kayla', clothing: 'top hat' });
 
@@ -61,21 +61,21 @@ it.skip('', function() {
     assert.equal(spell2, 'PULL RABBIT FROM TOP HAT');
   });
 
-it.skip('', function() {
+it('', function() {
     var magician = new Magician({ name: 'Leta', clothing: 'cape' });
 
     var spell = magician.performTrick();
 
-    assert.equal(spell, 'PULL DOVE FROM SLEEVE');
+    assert.equal(spell, 'PULL RABBIT FROM CAPE');
   });
 
-it.skip('', function() {
+it('', function() {
     var magician = new Magician({ name: 'Leta' });
 
     assert.equal(magician.confidencePercentage, 10);
   });
 
-it.skip('', function() {
+it('', function() {
     var magician = new Magician({ name: 'Leta' });
 
     magician.performTrick();
@@ -89,19 +89,23 @@ it.skip('', function() {
     assert.equal(magician.confidencePercentage, 50);
   });
 
-it.skip('', function() {
+it('', function() {
     var magician = new Magician({ name: 'Scott', assistant: true });
 
     assert.equal(magician.performShowStopper(), 'Oh no, this trick is not ready!');
 
-    for (var i = 0; i < 10; i++) {
+    for (var i = 0; i < 8; i++) {
       magician.performTrick();
     }
+
+    assert.equal(magician.performShowStopper(), 'Oh no, this trick is not ready!');
+
+    magician.performTrick();
 
     assert.equal(magician.performShowStopper(), 'WOW! The magician totally just sawed that person in half!');
   });
 
-it.skip('', function() {
+it('', function() {
     var magician1 = new Magician({ name: 'Scott', assistant: false });
     var magician2 = new Magician({ name: 'Hannah', assistant: true });
 
