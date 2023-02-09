41fac817d2d79960ad94eeaf5e54b22652d1508e
Heather Faerber <heather@turing.com>
2023-02-09 14:58:22 -0700

    Make Dragon functional

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,38 @@
+function createDragon(name, rider, temperment) {
+  var dragon = {
+    name: name,
+    rider: rider,
+    temperment: temperment,
+    hungry: true,
+    timesEaten: 0
+  }
+  return dragon
+}
 
+function greetRider(dragon) {
+  return `Hi, ${dragon.rider}!`
+}
+
+function eat(dragon) {
+  dragon.timesEaten += 1;
+  if (dragon.timesEaten >= 3) {
+    dragon.hungry = false
+  }
+  return dragon
+}
+
+// Spicy:
+function findFireBreathers(dragons) {
+  var aggresiveDragons = [];
+
+  for (var i=0; i<dragons.length; i++) {
+    if (dragons[i].temperment === 'aggressive') {
+      aggresiveDragons.push(dragons[i])
+    }
+  }
+  return aggresiveDragons
+}
+
+module.exports = {
+  createDragon, greetRider, eat, findFireBreathers
+}
