b1988a41d81d7537ec1b88baaff2807c135d4948
Jeremiah Black <jeremiahblackol@gmail.com>
2023-02-14 20:50:18 -0700

    want to look back and make sure it is a clear test suite for the first week

--- mythical-creatures/exercises/pegasus.js
+++ mythical-creatures/exercises/pegasus.js
@@ -1,7 +1,19 @@
-function createYourPegasus() {
+function createYourPegasus(namePlaceholder, colorPlaceholder, patternPlaceholder) {
+  var myPegasus = {
+    name: namePlaceholder,
+    color: colorPlaceholder,
+    pattern: patternPlaceholder,
+    canFly: true
+  }
 
+  return myPegasus
+};
+
+function makeAnEntrance(greeting, pegasus) {
+  return `${greeting}! My name is ${pegasus.name}, and I am a ${pegasus.color}, ${pegasus.pattern} coat Pegasus!`
 };
 
 module.exports = {
   createYourPegasus,
+  makeAnEntrance
 }
--- mythical-creatures/test/pegasus-test.js
+++ mythical-creatures/test/pegasus-test.js
@@ -1,14 +1,70 @@
 var assert = require('chai').assert;
-var {createYourPegasus} = require('../exercises/pegasus');
+var {createYourPegasus, makeAnEntrance} = require('../exercises/pegasus');
 
 describe('Pegasus', function() {
 
   it('should be a function', function() {
-    assert.isFunction(createYourPegasus);
+    assert.isFunction(createYourPegasus)
   });
 
-  it('should take any name you give it', function() {
-    var samplePegasus = 
-    
+  it('should be an object', function() {
+    var samplePegasus = createYourPegasus()
+
+    assert.typeOf(samplePegasus, "object")
+  });
+
+  it('should have any name you give it', function() {
+    var samplePegasus = createYourPegasus("Carlsbad Caveman")
+
+    assert.equal(samplePegasus.name, "Carlsbad Caveman")
+  });
+
+  it('should have a color and a pattern', function() {
+    var samplePegasusColor = "mustard yellow"
+    var samplePegasusPattern = "zebra stripes"
+    var samplePegasusName = "Spin That DJ"
+    var newSamplePegasus = createYourPegasus(samplePegasusName, samplePegasusColor, samplePegasusPattern)
+
+    assert.equal(newSamplePegasus.color, "mustard yellow")
+    assert.equal(newSamplePegasus.pattern, "zebra stripes")
+  });
+
+  it('should be able to have a different color and pattern', function() {
+    var newSamplePegasusColor = "cerulean"
+    var newSamplePegasusPattern = "leapord print"
+    var newSamplePegasusName = "Not Too Cool For School"
+    var newSamplePegasus = createYourPegasus(newSamplePegasusName, newSamplePegasusColor, newSamplePegasusPattern)
+
+    assert.equal(newSamplePegasus.color, newSamplePegasusColor)
+    assert.equal(newSamplePegasus.pattern, newSamplePegasusPattern)
+  });
+
+  it('should know how to fly by default', function() {
+    var newSamplePegasusColor = "burgundy"
+    var newSamplePegasusPattern = "snow cap"
+    var newSamplePegasusName = "I'm Medusa's Baby"
+    var newSamplePegasus = { 
+      name: newSamplePegasusName, 
+      color: newSamplePegasusColor, 
+      pattern: newSamplePegasusPattern,
+      canFly: true
+    }
+
+    assert.deepEqual(newSamplePegasus, createYourPegasus(newSamplePegasusName, newSamplePegasusColor, newSamplePegasusPattern))
+  });
+
+  it('should be able to make an entrance', function() {
+    var newSamplePegasusColor = "safety orange"
+    var newSamplePegasusPattern = "cape"
+    var newSamplePegasusName = "Who Do You Think You Are? I Am!"
+    var newSamplePegasus = createYourPegasus(newSamplePegasusName, newSamplePegasusColor, newSamplePegasusPattern)
+
+    assert.equal(newSamplePegasus.name, "Who Do You Think You Are? I Am!")
+    assert.equal(newSamplePegasus.color, "safety orange")
+    assert.equal(newSamplePegasus.pattern, "cape")
+
+    var greeting = "Hi"
+
+    assert.equal(makeAnEntrance(greeting, newSamplePegasus), "Hi! My name is Who Do You Think You Are? I Am!, and I am a safety orange, cape coat Pegasus!") 
   });
 });
