fddf4f4a2df0522f96350d10a967d67acb5796a0
Nick Teets <nickteets@gmail.com>
2023-02-24 15:19:52 -0700

    some cleanup on favorite foods

--- favorite-foods/favorite-foods-test.js
+++ favorite-foods/favorite-foods-test.js
@@ -2,7 +2,7 @@ var { createFavoriteFood } = require('./favorite-foods');
 var assert = require('chai').assert;
 
 describe('favorite foods', function () {
-   it('should create a new dish with a name', function () {
+   it.skip('should create a new dish with a name', function () {
       var pizza = createFavoriteFood({ dish: 'Pizza' });
 
       assert.equal(pizza.name, 'Pizza');
@@ -42,7 +42,7 @@ describe('favorite foods', function () {
 
       assert.equal(padThai.name, 'Pad Thai');
       assert.deepEqual(padThai.ingredients, ['Rice Noodles', 'Shrimp', 'Tofu', 'Egg', 'Bean Sprouts', 'Peanuts', 'Lime']);
-      assert.equal(pancakes.isSpicy, true);
+      assert.equal(padThai.isSpicy, true);
    });
 
    it.skip('should be able to taste the food and comment on how spicy it is', function () {
@@ -52,7 +52,7 @@ describe('favorite foods', function () {
       var comment1 = commentOnSpiciness(dish1);
       var comment2 = commentOnSpiciness(dish2);
   
-      assert.equal(comment1, 'Wow, this Pho so is spicy!');
+      assert.equal(comment1, 'Wow, this Pho is so spicy!');
       assert.equal(comment2, 'Phew, this Lasagna is not very spicy.');
    });
 
@@ -62,22 +62,22 @@ describe('favorite foods', function () {
       assert.equal(dish.timesOrdered, 0);
    });
 
-   it.skip('should be able to order favorite dishes', function () {
+   it.skip('should be able to order dishes', function () {
       var dish1 = createFavoriteFood({ dish: 'Fish and Chips', ingredients: ['Fish Fillet', 'Potatoes', 'Flour', 'Egg', 'Beer', 'Tartar Sauce'], isSpicy: false });
       var dish2 = createFavoriteFood({ dish: 'Chicken Curry', ingredients: ['Chicken', 'Coconut Milk', 'Onion', 'Garlic', 'Rice'], isSpicy: true });
       var dish3 = createFavoriteFood({ dish: 'Ice Cream', ingredients: ['Milk', 'Cream', 'Sugar', 'Vanilla Extract', 'Chocolate Chips'], isSpicy: false });
 
       var orderedDish1 = orderFood(dish1);
       var orderedDish2 = orderFood(dish2);
-      var orderedDish3 = orderDish(dish3);
+      var orderedDish3 = orderFood(dish3);
 
       assert.equal(orderedDish1.timesOrdered, 1);
       assert.equal(orderedDish2.timesOrdered, 1);
       assert.equal(orderedDish3.timesOrdered, 1);
 
-      var anotherOrder = orderDish(orderedDish3);
+      var anotherOrder = orderFood(orderedDish3);
 
-      assert.equal(anotherOrder.timesOrdered, 1);
+      assert.equal(anotherOrder.timesOrdered, 2);
    })
 
    // spicy 🌶️
@@ -85,7 +85,7 @@ describe('favorite foods', function () {
       var pizza = createFavoriteFood({ dish: 'Pizza', ingredients: ['Tomato Sauce', 'Cheese', 'Pepperoni', 'Mushrooms'], isSpicy: false });
       var smoothie = createFavoriteFood({ dish: 'Smoothie', ingredients: ['Banana', 'Strawberry', 'Blueberry', 'Milk', 'Honey'], isSpicy: false })
 
-      var list = createShoppingList(pizza, smoothie)
+      var list = createShoppingList([pizza, smoothie])
 
       assert.deepEqual(list, ['Tomato Sauce', 'Cheese', 'Pepperoni', 'Mushrooms', 'Banana', 'Strawberry', 'Blueberry', 'Milk', 'Honey'])
    });
