c3573b32e65b574cdd96fd60ef99e98eef18a04f
Kayla Wood <kaylaewood@gmail.com>
2023-03-21 08:16:23 -0600

    Fix airport tests

--- airport/airport-test.js
+++ airport/airport-test.js
@@ -1,46 +1,65 @@
 var assert = require('chai').assert;
-var { createElevator, changeFloors, dropOffPassenger } = require('./elevator');
+var { createAirport, welcomeGuests, landPlanes, checkAirlineLocations } = require('./airport');
 
-describe('Elevator', function() {
-  it.skip('should create an elevator', function() {
-    var elevator = createElevator('Empire State Building', 102, 5, ['Stacey', 'Javier', 'Tom']);
+describe('Airport', function() {
+  it.skip('should create an airport', function() {
+    var airport = createAirport('Denver International Airport', ['United', 'Southwest', 'Delta'], 144);
 
-    assert.equal(elevator.building, 'Empire State Building');
-    assert.equal(elevator.floors, 102);
-    assert.equal(elevator.currentFloor, 5);
-    assert.deepEqual(elevator.passengers, ['Stacey', 'Javier', 'Tom']);
+    assert.equal(airport.name, 'Denver International Airport');
+    assert.equal(airport.availableGates, 144);
+    assert.equal(airport.airlines[0], 'United');
   });
 
-  it.skip('should be able to change floors', function() {
-    var elevator = createElevator('Empire State Building', 102, 5, []);
+  it.skip('should welcome people to the airport', function() {
+    var denverAirport = createAirport('Denver International Airport', ['United', 'Southwest', 'Delta'], 144);
+    var sanDiegoAirport = createAirport('San Diego International Airport', ['Frontier', 'American'], 48);
 
-    var message = changeFloors(elevator, 10);
+    
+    var denverWelcome = welcomeGuests(denverAirport);
+    var sanDiegoWelcome = welcomeGuests(sanDiegoAirport);
 
-    assert.equal(message, 'Taking you to floor 10!');
+    assert.equal(denverWelcome, 'Welcome to Denver International Airport!');
+    assert.equal(sanDiegoWelcome, 'Welcome to San Diego International Airport!');
   });
 
-  it.skip('should not be able to change floors if already on that floor', function() {
-    var elevator = createElevator('West High School', 3, 2, ['Sarah', 'Ari']);
+  it.skip('should keep track of open gates', function() {
+    var bakersfieldAirport = createAirport('Meadows Field Airport', ['United', 'American'], 12);
+    var sanDiegoAirport = createAirport('San Diego International Airport', ['Frontier', 'American'], 48);
 
-    var message = changeFloors(elevator, 2);
+    landPlanes(bakersfieldAirport, 11);
+    landPlanes(sanDiegoAirport, 2);
 
-    assert.equal(message, 'You\'re already on floor 2!');
+    assert.equal(bakersfieldAirport.availableGates, 1);
+    assert.equal(sanDiegoAirport.availableGates, 46);
   });
 
-  it.skip('should not be able to take you to a floor that does not exist', function() {
-    var elevator = createElevator('West High School', 3, 2, ['Katherine', 'Erika']);
+  it.skip('should not be able to occupy more gates than available', function() {
+    var columbusAiport = createAirport('John Glenn Airport', ['Southwest', 'Frontier'], 24);
 
-    var message = changeFloors(elevator, 100);
+    var occupiedGates1 = landPlanes(columbusAiport, 22);
 
-    assert.equal(message, 'Floor 100 does not exist!');
+    assert.equal(columbusAiport.availableGates, 2);
+    assert.equal(occupiedGates1, 'Success! Current availability is 2.')
+
+    var occupiedGates2 = landPlanes(columbusAiport, 3);
+
+    assert.equal(columbusAiport.availableGates, 0);
+    assert.equal(occupiedGates2, 'Oh no! Not enough gates available. Current overflow is 1.')
   });
 
-  it.skip('should be able to drop off passenger', function() {
-    var elevator = createElevator('Upper Valley Mall', 4, 1, ['Scott', 'Mark', 'Joey']);
+  it.skip('should be able to tell you where an airline flies to', function() {
+    var columbusAiport = createAirport('John Glenn Airport', ['Southwest', 'Frontier'], 24);
+    var bakersfieldAirport = createAirport('Meadows Field Airport', ['United', 'American'], 12);
+    var sanDiegoAirport = createAirport('San Diego International Airport', ['Frontier', 'American'], 48);
+    var denverAirport = createAirport('Denver International Airport', ['United', 'Southwest', 'Delta'], 144);
+    var allAirports = [columbusAiport, bakersfieldAirport, sanDiegoAirport, denverAirport];
 
-    var remainingPassengers = dropOffPassenger(elevator, 'Mark');
+    var southwestCarriers = checkAirlineLocations(allAirports, 'Southwest');
+    var unitedCarriers = checkAirlineLocations(allAirports, 'United');
 
-    assert.deepEqual(remainingPassengers, ['Scott', 'Joey']);
+    assert.deepEqual(southwestCarriers, ['John Glenn Airport', 'Denver International Airport']);
+    assert.deepEqual(unitedCarriers, ['Meadows Field Airport', 'Denver International Airport']);
   });
+
 
 });
