bdc4faeef557a39684aaf33c43b3e8c1bb3e20c8
Jason Alberto <alberto.jason@gmail.com>
2023-03-27 19:09:53 -0400

    complete dragon testing

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,8 +1,46 @@
+function createDragon(name, rider, temperment) {
+  var dragon = {};
+  dragon.name = name;
+  dragon.rider = rider;
+  dragon.temperment = temperment;
+  dragon.timesEaten = 0;
+  dragon.hungry = true;
 
+  return dragon; 
+}
+
+function greetRider(dragon) {
+  return `Hi, ${dragon.rider}!`;
+}
+
+function eat(dragon) {
+
+  if (dragon.timesEaten < 3) {
+    dragon.timesEaten++;
+   } 
+  
+  if (dragon.timesEaten === 3) {
+    dragon.hungry = false;
+  }
+
+  return dragon;
+}
+
+function findFireBreathers(allDragons) {
+  var fireBreather = [];
+
+  for (var i = 0; i < allDragons.length; i++) {
+    if (allDragons[i].temperment === 'aggressive') {
+      fireBreather.push(allDragons[i]);
+    }
+  }
+
+  return fireBreather;
+}
 
 module.exports = {
-  // createDragon, 
-  // greetRider, 
-  // eat, 
-  // findFireBreathers
+  createDragon, 
+  greetRider, 
+  eat,
+  findFireBreathers
 }
