6e11845bc033f140ccf64e5849a813b884967d92
Jamie Caudill <j.caudill7177@gmail.com>
2023-03-27 16:50:24 -0700

    Write createDragon function and pass all tests

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,8 +1,23 @@
 
 
 module.exports = {
-  // createDragon, 
+  createDragon, 
   // greetRider, 
   // eat, 
   // findFireBreathers
 }
+
+// goal: write a function that creates an object of dragons with name, rider, and temperment
+
+// input: three parameters: name, rider, temperment
+// output: object
+
+function createDragon(name, rider, temperment) {
+  var dragon = {
+    name: name,
+    rider: rider,
+    temperment: temperment
+  }
+
+  return dragon;
+};
