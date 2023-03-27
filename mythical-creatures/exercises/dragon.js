1afa1b21f4fd3cfef1d4b1aab7ddd9b065631b7b
Denzel Turambi <dturambi26@gmail.com>
2023-03-27 16:51:29 -0600

    Add all functions

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,8 +1,40 @@
+function createDragon(name, rider, temperment) {
+  dragonInfo = {
+    name,
+    rider,
+    temperment,
+    timesEaten: 0,
+    hungry: true
+  };
+  return dragonInfo;
+};
 
+function greetRider(dragonInfo) {
+  return `Hi, ${dragonInfo.rider}!`
+}
+
+function eat(dragonInfo) {
+  dragonInfo.timesEaten += 1;
+  if (dragonInfo.timesEaten >= 3) {
+    dragonInfo.hungry = false
+  }
+  return dragonInfo;
+}
+
+function findFireBreathers(allDragons) {
+  var fireBreathers = [];
+  for (var i = 0; i < allDragons.length; i++) {
+    if (dragonInfo.temperment === 'aggressive') {
+      fireBreathers.push(allDragons[i]);
+    }
+  }
+  console.log(allDragons)
+  return fireBreathers
+}
 
 module.exports = {
-  // createDragon, 
-  // greetRider, 
-  // eat, 
-  // findFireBreathers
+  createDragon, 
+  greetRider, 
+  eat, 
+  findFireBreathers
 }
