824bf1d190c6bae4dc59b5ae3f52a3099df02ee5
Alec Livaditis <aleclivaditis@gmail.com>
2023-03-27 20:52:06 -0400

    Add dragon test

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,8 +1,39 @@
+function createDragon(name, rider, temperment) {
+  var dragon = {
+    name,
+    rider,
+    temperment,
+    timesEaten: 0,
+    hungry: true
+  }
+  return dragon
+}
 
+function greetRider(dragon) {
+  return `Hi, ${dragon.rider}!`
+}
+
+function eat(dragon) {
+  dragon.timesEaten++
+  if (dragon.timesEaten >= 3) {
+    dragon.hungry = false
+  }
+  return dragon
+}
+
+function findFireBreathers(allDragons) {
+  var fireBreathers = []
+  for ( i = 0; i < allDragons.length; i++) {
+    if (allDragons[i].temperment === 'aggressive') {
+      fireBreathers.push(allDragons[i])
+    }
+  }
+  return fireBreathers
+}
 
 module.exports = {
-  // createDragon, 
-  // greetRider, 
-  // eat, 
-  // findFireBreathers
+  createDragon, 
+  greetRider, 
+  eat, 
+  findFireBreathers
 }
