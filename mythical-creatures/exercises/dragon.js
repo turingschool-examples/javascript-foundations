8a2bc42b3b28472a2a287a1b59c91d227481cf15
Calli Herrmann <calliherrmann@gmail.com>
2023-03-28 16:54:27 -0400

    Take out comments from dragon file and add to the hobbit celebrateBirthday function.

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -13,11 +13,6 @@ function greetRider(dragon) {
   return `Hi, ${dragon.rider}!`
 };
 
-// function eat() takes in the dragon object after it's been created.
-// creates a new variable called fedDragon using eat()
-// function eat() alters the dragon object keys timesEaten and 
-// hungry.
-
 function eat(dragon) {
   dragon.timesEaten += 1;
   if (dragon.timesEaten >= 3){
@@ -26,9 +21,6 @@ function eat(dragon) {
   return dragon;
 };
 
-// takes in an array of dragons
-// iterate through dragons temperment to find 'aggressive'
-// return new array of aggressive dragons
 function findFireBreathers(allDragons) {
   var meanDragons = [];
   for (var i = 0; i < allDragons.length; i++) {
--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -2,7 +2,7 @@ function createHobbit(name, age) {
   var hobbit = {
     name: name ? name : 'unknown',
     age: age ? age : 0,
-    isAdult: age < 32 ? false : true,
+    isAdult: age <= 32 ? false : true,
     isOld: age > 100 ? true : false,
   } 
   return hobbit;
@@ -11,13 +11,20 @@ function createHobbit(name, age) {
 
 function celebrateBirthday(hobbit) {
   hobbit.age += 1;
+  if (hobbit.age <= 32) {
+    hobbit.isAdult = false
+  } else {
+    hobbit.isAdult = true
+  };
   if (hobbit.age >= 101) {
     hobbit.isOld = true
   } else {
     hobbit.isOld = false
   }
   return hobbit;
-}
+};
+
+
 
 module.exports = {
   createHobbit, 
