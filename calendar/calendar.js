8464865a03e80fe459f527dcbe28b0b560ca2b32
Jason Alberto <alberto.jason@gmail.com>
2023-03-31 15:09:06 -0400

    complete calendar testing challenge

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -1,3 +1,36 @@
+function createEvent(inputTask, inputMonth, inputDate) {
+    if (!inputDate || inputDate === 0 || inputDate > 31) {
+        return `Error: ${inputDate} is not a valid day`;
+    }
+    
+    var event = {};
+    event.title = inputTask;
+    event.month = inputMonth;
+    event.day = inputDate;
+    return event;
+}
 
+function createCalendar(inputOwner, inputEvents) {
+    var calendar = {
+        owner: inputOwner,
+        events: inputEvents
+    };
+    return calendar;
+}
 
-module.exports = {  };
+function reportMonthlyEvents(calendar, month) {
+    var monthlyEvents = [];
+    for (var i = 0; i < calendar.events.length; i++) {
+        if (calendar.events[i].month === month) {
+            monthlyEvents.push(calendar.events[i]);
+        }
+    }
+
+    return monthlyEvents;
+}
+
+module.exports = {
+    createEvent,
+    createCalendar,
+    reportMonthlyEvents
+};
