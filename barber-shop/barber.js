5cac62bd944f272d48fdb5a7c8e890f29fd73f17
Jason Alberto <alberto.jason@gmail.com>
2023-04-02 14:38:42 -0400

    Complete barber challenge testing

--- barber-shop/barber.js
+++ barber-shop/barber.js
@@ -1,3 +1,39 @@
+function createBarber(barberName, totalEarnings, haircutStyle) {
+    if (!totalEarnings && !haircutStyle) {
+        totalEarnings = 0;
+        haircutStyle = [];
+    }
 
+    return barber = {
+        name: barberName,
+        earnings: totalEarnings, 
+        haircuts: haircutStyle
+    }
+}
 
-module.exports = {  };
+function giveCompliment(haircutStyle) {
+    return `This ${haircutStyle.style} looks great!`;
+}
+
+function cutHair(barber, haircutStyle) {
+    barber.haircuts.push(haircutStyle);
+    barber.earnings += haircutStyle.price;
+    return barber;
+}
+
+function listStyles(barber, knownCuts) {
+    var hairLength = [];
+    for (var i = 0; i < barber.haircuts.length; i++) {
+        if (barber.haircuts[i].hairLength === knownCuts)
+            hairLength.push(barber.haircuts[i].style);
+    }
+
+    return hairLength;
+}
+
+module.exports = {
+    createBarber,
+    giveCompliment,
+    cutHair,
+    listStyles
+};
