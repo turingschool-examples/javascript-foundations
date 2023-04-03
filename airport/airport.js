52322b80d4f94617dced5fa4a6f284a90920b604
Denzel Turambi <dturambi26@gmail.com>
2023-04-03 12:56:01 -0600

    Add all the functions, still solving the last one

--- airport/airport.js
+++ airport/airport.js
@@ -1,9 +1,33 @@
+function createAirport(name, airlines, availableGates) {
+  var airport = {
+    name,
+    airlines,
+    availableGates,
+  }
+  return airport;
+};
 
+function welcomeGuests(airport) {
+  return `Welcome to ${airport.name}!`
+}
 
+function landPlanes(airport, planesLandedCt) {
+  airport.availableGates -= planesLandedCt;
+  if (airport.availableGates > 0) {
+    return `Success! Current availability is ${airport.availableGates}.`
+  } else if (airport.availableGates < 0){
+    airport.availableGates = 0;
+    return `Oh no! Not enough gates available. Current overflow is 1.`
+  }
+}
+
+function checkAirlineLocations(airport, airline) {
+
+}
 
 module.exports = { 
-  // createAirport, 
-  // welcomeGuests, 
-  // landPlanes, 
-  // checkAirlineLocations
+  createAirport, 
+  welcomeGuests, 
+  landPlanes, 
+  checkAirlineLocations
 };
