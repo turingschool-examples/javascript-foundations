5a7114f17d1101f85f1f1612ad3b659c2e2ce60a
Jamie Caudill <j.caudill7177@gmail.com>
2023-04-03 16:35:36 -0700

    Pass all tests for airport

--- airport/airport.js
+++ airport/airport.js
@@ -2,8 +2,45 @@
 
 
 module.exports = { 
-  // createAirport, 
-  // welcomeGuests, 
-  // landPlanes, 
-  // checkAirlineLocations
+  createAirport, 
+  welcomeGuests, 
+  landPlanes, 
+  checkAirlineLocations
 };
+
+function createAirport(location, airlines, gates) {
+  var airport = { 
+    name: location,
+    availableGates: gates,
+    airlines: airlines
+  }
+  return airport;
+}
+
+function welcomeGuests(airport) {
+  return `Welcome to ${airport.name}!`
+}
+
+function landPlanes(airport, planes) {
+  airport.availableGates = airport.availableGates - planes;
+  if (airport.availableGates > 0) {
+    return `Success! Current availability is ${airport.availableGates}.`;
+  }
+  if (airport.availableGates <= 0) {
+    var overflow = airport.availableGates * -1;
+    airport.availableGates = 0;
+    return `Oh no! Not enough gates available. Current overflow is ${overflow}.`
+  }
+} 
+
+function checkAirlineLocations(airports, airline) {
+  var carriers = [];
+  for (var i = 0; i < airports.length; i++) {
+    for (var j = 0; j < airports[i].airlines.length; j++) {
+      if (airline === airports[i].airlines[j]) {
+        carriers.push(airports[i].name)
+      }
+    }
+  }
+  return carriers;
+}
