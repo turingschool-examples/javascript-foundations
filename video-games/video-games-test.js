825896a79f112de95413d9107955ea30e7e4465f
Calli Herrmann <calliherrmann@gmail.com>
2023-04-03 10:53:37 -0400

    Initial commit

--- video-games/video-games-test.js
+++ video-games/video-games-test.js
@@ -1,15 +1,15 @@
-var { createPlayer } = require('./video-games');
+var { createPlayer, createLevel } = require('./video-games');
 var assert = require('chai').assert;
 
 describe('video games', function () {
-   it.skip('should create a new player with a name and an age', function () {
+   it('should create a new player with a name and an age', function () {
       var player1 = createPlayer('Ace Skateboarder', 19);
 
       assert.equal(player1.name, 'Ace Skateboarder');
       assert.equal(player1.age, 19);
    });
 
-   it.skip('should create another player with a name and an age', function () {
+   it('should create another player with a name and an age', function () {
       var player1 = createPlayer('Guitar Heroine', 25);
       var player2 = createPlayer('Time Traveler', 35);
 
@@ -19,7 +19,7 @@ describe('video games', function () {
       assert.equal(player2.age, 35);
    });
 
-   it.skip('should create a new player with a unique moveset', function () {
+   it('should create a new player with a unique moveset', function () {
       var player1 = createPlayer('Super Skater', 27, ['super jump', 'board smash', 'grind rail']);
 
       assert.equal(player1.name, 'Super Skater');
@@ -27,13 +27,13 @@ describe('video games', function () {
       assert.deepEqual(player1.moveset['super jump', 'board smash', 'grind rail']);
    });
 
-   it.skip('should be able to create a new level with a name', function () {
+   it('should be able to create a new level with a name', function () {
       var level = createLevel('Island Oasis');
 
       assert.equal(level.name, 'Island Oasis');
    });
 
-   it.skip('should be able to create a new level with a name and a player', function () {
+   it('should be able to create a new level with a name and a player', function () {
       var player1 = createPlayer('The Future Savior', 18, ['time travel', 'combat training', 'futuristic weapon']);
       var level = createLevel('Zombie Infested City', [player1]);
 
@@ -41,7 +41,7 @@ describe('video games', function () {
       assert.deepEqual(level.players, [player1]);
    });
 
-   it.skip('should be able to create a new level with a name and many players', function () {
+   it('should be able to create a new level with a name and many players', function () {
       var player1 = createPlayer('Rock Star Rebel', 21, ['power chord', 'stage dive', 'crowd surf']);
       var player2 = createPlayer('Pro Skater Prodigy', 16, ['360 flip', 'board slide', 'gap transfer']);
       var level = createLevel('Candy Cane Canyon', [player1, player2]);
@@ -50,7 +50,7 @@ describe('video games', function () {
       assert.deepEqual(level.players, [player1, player2]);
    });
 
-   it.skip('should initiate a level with a player and 3 lives and 0 coins', function () {
+   it('should initiate a level with a player and 3 lives and 0 coins', function () {
       var player1 = createPlayer('Quantum Jumper', 30, ['dimension jump', 'portal creation', 'alternate reality explosion']);
       var level = createLevel('Carnival of Terror', [player1]);
 
--- video-games/video-games.js
+++ video-games/video-games.js
@@ -1,4 +1,24 @@
+function createPlayer(name, age, moveset) {
+    var player = {
+        name: name,
+        age: age,
+        moveset: moveset,
+    }
+    return player;
+}
 
+function createLevel(name, players) {
+    var level = {
+        name: name,
+        players: players,
+        lives: 3,
+        coins: 0
+    }
+    return level;
+}
+console.log(createLevel('Island Oasis'))
 
-
-module.exports = { };
+module.exports = {
+    createPlayer,
+    createLevel
+ };
