fd14a740660d14e124665c95276727f4658dc7bc
Jamie Caudill <j.caudill7177@gmail.com>
2023-04-03 16:05:27 -0700

    Pass all tests for birthdays

--- birthdays/birthdays.js
+++ birthdays/birthdays.js
@@ -1,3 +1,30 @@
+function createBirthday(name, month, day) {
+    var birthdays = {
+        name: name,
+        month: month,
+        day: day,
+    }
+    return birthdays;
+}
 
+function celebrateBirthday (birthday){
+var name = birthday.name
+var month = birthday.month
+var day = birthday.day
+return `Today is ${month}/${day}! Happy birthday, ${name}!`
+}
 
-module.exports = {  };
+function countBirthdays(birthdays, month) {
+    birthdaysCounter = 0;
+    for (var i = 0; i < birthdays.length; i++) {
+        if (birthdays[i].month === month) {
+            birthdaysCounter++;
+        }
+    }
+    return birthdaysCounter;
+}
+module.exports = { 
+    createBirthday,
+    celebrateBirthday,
+    countBirthdays
+ };
