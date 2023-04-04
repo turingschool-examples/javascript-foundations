13cc18cef16e2703cf8bb7d7a5466cbecb743343
Rachel Prather <rachelprather1@gmail.com>
2023-04-03 17:21:58 -0700

    Add functions to create hobbit, age, change age category

--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -1,8 +1,38 @@
+function createHobbit(name = 'unknown', age = 0, isAdult = true, isOld = true) {
+  var hobbit = {
+    name: name,
+    age: age,
+    isAdult: isAdult,
+    isOld: isOld,
+  }
 
+  if (hobbit.age < 33) {
+    hobbit.isAdult = false;
+    hobbit.isOld = false;
+  } else if (hobbit.age < 101) {
+    hobbit.isOld = false;
+  }
+  
+  return hobbit;
+}
+ 
+function celebrateBirthday(hobbit) {
+  hobbit.age += 1;
+
+  if (hobbit.age > 32) {
+    hobbit.isAdult = true;
+  }
+
+  if (hobbit.age > 100) {
+    hobbit.isOld = true;
+  }
+
+  return hobbit;
+}
 
 module.exports = {
-  // createHobbit, 
-  // celebrateBirthday, 
+  createHobbit, 
+  celebrateBirthday, 
   // getRing, 
   // meetPeople, 
   // findFriends
