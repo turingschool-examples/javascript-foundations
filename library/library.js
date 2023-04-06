b6d956c79f25d13bb419cbe720cb296de969601d
Calli Herrmann <calliherrmann@gmail.com>
2023-04-06 10:54:45 -0400

    Complete library

--- library/library.js
+++ library/library.js
@@ -1,3 +1,28 @@
+function issueCard(name, age) {
+    var card = {
+        name: name,
+        age: age,
+        numBooksCheckedOut: 0,
+        isChild: (age < 12) ? true : false,
+    }
+    return card;
+}
 
+function searchByAuthor(books, author) {
+    var foundBooks = [];
+    for (var i = 0; i < books.length; i++) {
+        if (books[i].author === author){
+            foundBooks.push(books[i])
+        }
+    }
+    if (!foundBooks.length){
+        return 'No book found for search criteria';
+    } else {
+        return foundBooks;
+    }
+}
 
-module.exports = { };
+module.exports = {
+    issueCard,
+    searchByAuthor,
+};
