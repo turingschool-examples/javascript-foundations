32cf75349e988c7587353f55d7954a3ff83761ed
Jamie Caudill <j.caudill7177@gmail.com>
2023-04-06 08:44:06 -0700

    Write cutHair function

--- barber-shop/barber.js
+++ barber-shop/barber.js
@@ -1,3 +1,33 @@
 
 
-module.exports = {  };
+module.exports = {  
+    createBarber, 
+    giveCompliment,
+    cutHair
+};
+
+function createBarber(name, earnings, cuts) {
+    var barber = {
+        name: name,
+        earnings: earnings,
+        haircuts: cuts
+    }
+    if (earnings === undefined) {
+        barber.earnings = 0;
+    }
+    if (cuts === undefined) {
+        barber.haircuts = [];
+    }
+    return barber;
+}
+
+function giveCompliment(cut) {
+    return `This ${cut.style} looks great!`;
+}
+
+function cutHair(barber, haircut) {
+    // take haircut and push it into haircuts array
+    barber.haircuts.push(haircut);
+    return barber;
+}
+
