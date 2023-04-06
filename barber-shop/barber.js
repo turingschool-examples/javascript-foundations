954435ef6d8cd1e89e15d2f1aea36bb144429394
Denzel Turambi <dturambi26@gmail.com>
2023-04-06 13:02:50 -0600

    Add createBarber and giveCompliment function

--- barber-shop/barber.js
+++ barber-shop/barber.js
@@ -1,3 +1,17 @@
+function createBarber(name, earnings, haircuts) {
+    var barber = {
+        name,
+        earnings: earnings || 0,
+        haircuts: haircuts || [],
+    }
+    return barber;
+}
 
+function giveCompliment(cutType) {
+    
+}
 
-module.exports = {  };
+module.exports = {  
+    createBarber,
+    giveCompliment
+};
