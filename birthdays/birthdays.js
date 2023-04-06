8b4041f1d1a127686889ff4e84bb79c5071e42a6
Calli Herrmann <calliherrmann@gmail.com>
2023-04-05 20:16:07 -0400

    Complete birthdays

--- birthdays/birthdays.js
+++ birthdays/birthdays.js
@@ -1,3 +1,28 @@
+function createBirthday(name, month, day) {
+    var person = {
+        name: name,
+        month: month,
+        day: day
+    }
+    return person;
+}
 
+function celebrateBirthday(person) {
+    var birthdayMessage = `Today is ${person.month}/${person.day}! Happy birthday, ${person.name}!`
+    return birthdayMessage;
+}
 
-module.exports = {  };
+function countBirthdays(birthdays, month) {
+    var count = 0;
+    for (var i = 0; i < birthdays.length; i++){
+        if (birthdays[i].month === month){
+            count += 1;
+        }
+    } return count;
+}
+
+module.exports = {
+    createBirthday,
+    celebrateBirthday,
+    countBirthdays,
+  };
