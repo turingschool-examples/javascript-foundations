6227fe774d0d7452ad4d5d31099b02fb4a3ce4e0
Calli Herrmann <calliherrmann@gmail.com>
2023-04-05 20:50:39 -0400

    Complete favorite foods

--- favorite-foods/favorite-foods.js
+++ favorite-foods/favorite-foods.js
@@ -1,4 +1,39 @@
+function createFavoriteFood(food) {
+    var faveFood = {
+        name: food.dish,
+        ingredients: food.ingredients,
+        isSpicy: food.isSpicy,
+        timesOrdered: 0,
+    }
+    return faveFood;
+}
 
+function commentOnSpiciness(food) {
+    if (food.isSpicy === true) {
+        return `Wow, this ${food.name} is so spicy!`
+    } else {
+        return `Phew, this ${food.name} is not very spicy.`
+    }
+}
 
+function orderFood(food) {
+    food.timesOrdered += 1;
+    return food;
+}
 
-module.exports = { };
+function createShoppingList(foods) {
+    var list = [];
+    for (var i = 0; i < foods.length; i++) {
+        var currentFood = foods[i];
+        var currentIngredients = currentFood.ingredients;
+        list = list.concat(currentIngredients);
+    }
+    return list;
+}
+
+module.exports = {
+    createFavoriteFood,
+    commentOnSpiciness,
+    orderFood,
+    createShoppingList,
+ };
