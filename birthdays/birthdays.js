4b2044da6c3039fa2c583ecb1dd052d0b628fd3a
Jason Alberto <alberto.jason@gmail.com>
2023-04-10 22:07:05 -0400

    Complete birthday testing challenges

--- birthdays/birthdays.js
+++ birthdays/birthdays.js
@@ -1,3 +1,27 @@
+function createBirthday(name, month, day) {
+    return {
+        name: name,
+        month: month,
+        day: day
+    }
+}
 
+function celebrateBirthday(birthday) {
+    return `Today is ${birthday.month}/${birthday.day}! Happy birthday, ${birthday.name}!`;
+}
 
-module.exports = {  };
+function countBirthdays(birthday, month) {
+    var count = 0;
+    for (var i = 0; i < birthday.length; i++) {
+        if (birthday[i].month === month) {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+module.exports = {
+    createBirthday,
+    celebrateBirthday,
+    countBirthdays
+};
--- dollar-store-vending-machine/index.js
+++ dollar-store-vending-machine/index.js
@@ -1,2 +1,32 @@
+function createItemStock(name, quantity, price) {
+    return {
+        name: name || 'unknown',
+        quantity: quantity || 0, 
+        price: price || 1.00
+    }
+}
 
-module.exports = {}
+function makePurchase(selectedItem, moneyForPurchase) {
+    if (selectedItem.quantity === 0) {
+        return 'Sorry, there are none left';
+    } else if (selectedItem.price > moneyForPurchase) {
+        return `Sorry, you need at least $${selectedItem.price} to make that purchase`;
+    } else {
+        return `Here are your ${selectedItem.name}`;
+    }
+}
+
+function collectChange(change) {
+    var accumulator = 0;
+    for (var i = 0; i < change.length; i++) {
+        accumulator += change[i];
+    }
+    return accumulator;
+}
+
+
+module.exports = {
+    createItemStock,
+    makePurchase,
+    collectChange
+}
