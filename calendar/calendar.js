a87c1da6a1f3276983f776d43057034e4ed7f1c5
Denzel Turambi <dturambi26@gmail.com>
2023-04-13 13:35:38 -0600

    Solve and complete calender test

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -1,3 +1,36 @@
+function createEvent(title, month, day) {
+    var calenderEvent = {
+        title,
+        month,
+        day
+    }
+    if (calenderEvent.day > 31 || calenderEvent.day <= 0){
+        return `Error: ${calenderEvent.day} is not a valid day`
+    }
+    return calenderEvent;
+}
 
+function createCalendar(owner, events) {
+    var calender = {
+        owner,
+        events
+    }
+    return calender;
+}
 
-module.exports = {  };
+function reportMonthlyEvents(calender, month) {
+    console.log(calender);
+    var monthlyEvents = [];
+    for (var i = 0; i < calender.events.length; i++) {
+        if (calender.events[i].month === month) {
+            monthlyEvents.push(calender.events[i]);
+        }
+    }
+    return monthlyEvents;
+}
+
+module.exports = {
+    createEvent,
+    createCalendar,
+    reportMonthlyEvents
+  };
