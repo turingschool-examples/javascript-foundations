2afb3fc354bbfcfaecf845e2ad8fa4d8fce0b5cb
Rachel Prather <rachelprather1@gmail.com>
2023-04-14 13:56:37 -0700

    Complete birthday tests

--- birthdays/birthdays.js
+++ birthdays/birthdays.js
@@ -1,3 +1,31 @@
+function createBirthday(name, month, day) {
+	return birthday = {
+		name: name,
+		month: month,
+		day: day
+	}
+}
 
+function celebrateBirthday(birthday) {
+	return `Today is ${birthday.month}/${birthday.day}! Happy birthday, ${birthday.name}!`
+}
 
-module.exports = {  };
+function countBirthdays(birthdays, month) {
+	var birthdayCount = [];
+	for (var i = 0; i < birthdays.length; i++) {
+		if (birthdays[i].month === month) {
+			birthdayCount.push(birthdays[i]);
+		}
+	}
+	return birthdayCount.length;
+}
+
+// iterate over array
+// if month === birthdays[i].month, push into new array
+// return lenght of new array
+
+module.exports = { 
+	createBirthday,
+	celebrateBirthday,
+	countBirthdays
+ };
