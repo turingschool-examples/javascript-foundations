06a810f2206e2b9b2650f7b8af058cd9a6974368
Jamie Caudill <j.caudill7177@gmail.com>
2023-04-15 16:10:36 -0700

    Pass all tests for favorite-foods

--- favorite-foods/favorite-foods.js
+++ favorite-foods/favorite-foods.js
@@ -1,4 +1,41 @@
 
 
 
-module.exports = { };
+module.exports = {
+  createFavoriteFood,
+  commentOnSpiciness,
+  orderFood, 
+  createShoppingList
+ };
+
+ function createFavoriteFood(dish) {
+  var favoriteFood = {
+    name: dish.dish,
+    ingredients: dish.ingredients,
+    isSpicy: dish.isSpicy,
+    timesOrdered: 0
+  }
+  return favoriteFood;
+ }
+
+ function commentOnSpiciness(dish) {
+  if (dish.isSpicy) {
+    return `Wow, this ${dish.name} is so spicy!`
+  }
+  return `Phew, this ${dish.name} is not very spicy.`
+ }
+
+function orderFood(dish) {
+  dish.timesOrdered++;
+  return dish;
+}
+
+function createShoppingList(dishes) {
+  var list = [];
+  for (var i = 0; i < dishes.length; i++) {
+    for (var j = 0; j < dishes[i].ingredients.length; j++) {
+      list.push(dishes[i].ingredients[j]);
+    }
+  }
+  return list;
+}
