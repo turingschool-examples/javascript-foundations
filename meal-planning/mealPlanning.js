f28002c8de4ce0811c2b43d3e18b74aa2ed969e6
Jamie Caudill <j.caudill7177@gmail.com>
2023-04-15 16:30:55 -0700

    Pass all tests for meal-planning

--- meal-planning/mealPlanning.js
+++ meal-planning/mealPlanning.js
@@ -1,2 +1,31 @@
 
-module.exports = { }
+module.exports = { 
+  createMeal,
+  addDish,
+  calculateCalories
+}
+
+function createMeal(meal, calories) {
+  var meal = {
+    type: meal,
+    calorieGoal: calories,
+    dishes: []
+  }
+  return meal;
+}
+
+function addDish(meal, addMeal) {
+  if (addMeal.calories < meal.calorieGoal) {
+    meal.dishes.push(addMeal);
+    meal.calorieGoal = meal.calorieGoal - addMeal.calories;
+  }  
+  return meal;
+}
+
+function calculateCalories(meal) {
+  calories = 0;
+  for (var i = 0; i < meal.dishes.length; i++) {
+    calories = calories + meal.dishes[i].calories;
+  }
+  return `${meal.type} has a total of ${calories} calories.`;
+}
