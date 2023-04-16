5067b940a0e6e595c1966029f5cfc1d835cada78
Rachel Prather <rachelprather1@gmail.com>
2023-04-16 14:55:42 -0700

    Complete elevator tests

--- elevator/elevator.js
+++ elevator/elevator.js
@@ -1,3 +1,40 @@
+function createElevator(building, floors, currentFloor, passengers) {
+	return elevator = {
+		building: building,
+		floors: floors,
+		currentFloor: currentFloor,
+		passengers: passengers
+	}
+}
 
+function changeFloors(elevator, floor) {
+	if (elevator.currentFloor === floor) {
+		return `You\'re already on floor ${floor}!`
+	} else if (floor > elevator.floors) {
+		return `Floor ${floor} does not exist!`
+	} else {
+		elevator.currentFloor = floor;
+		return `Taking you to floor ${floor}!`;
+	}
+}
 
-module.exports = {  };
+function dropOffPassenger(elevator, passenger) {
+	for (var i = 0; i < elevator.passengers.length; i++) {
+		if (elevator.passengers[i] === passenger) {
+			elevator.passengers.splice(i, 1);
+		}
+	}
+	
+	return elevator.passengers;
+}
+
+// iterate over array of passengers
+// if passenger equals the current array passenger, splice 1 item at that location
+// return the array
+	
+
+module.exports = { 
+	createElevator,
+	changeFloors,
+	dropOffPassenger
+ };
