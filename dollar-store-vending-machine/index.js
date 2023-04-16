9c1bb50582c588960230f14533b5f5ccc5f4acf8
Rachel Prather <rachelprather1@gmail.com>
2023-04-16 14:46:09 -0700

    Complete vending machine tests

--- dollar-store-vending-machine/index.js
+++ dollar-store-vending-machine/index.js
@@ -1,2 +1,33 @@
+function createItemStock(name = 'unknown', quantity = 0, price = 1.00) {
+	return item = {
+		name: name,
+		quantity: quantity,
+		price: price
+	}
+}
 
-module.exports = {}
+function makePurchase(item, amountMoney) {
+	if (amountMoney < item.price) {
+		return `Sorry, you need at least $${item.price} to make that purchase`;
+	} else if (!item.quantity) {
+		return `Sorry, there are none left`;
+	} else {
+		return `Here are your ${item.name}`;
+	}
+}
+
+function collectChange(coins) {
+	var total = 0;
+	for (var i = 0; i < coins.length; i++) {
+		total += coins[i];
+	}
+	
+	return total; 
+}
+
+
+module.exports = {
+	createItemStock,
+	makePurchase,
+	collectChange
+}
