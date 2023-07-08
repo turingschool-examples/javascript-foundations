577e5ee6e7ce816e345608671c30c5b8ac5a5521
Matt Campbell <ematthewcampbell@gmail.com>
2023-07-08 13:01:28 -0500

    finished dragons

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,8 +1,39 @@
+function createDragon(name, rider, temperment, timesEaten = 0, hungry = true) {
+  return {
+    name,
+    rider,
+    temperment,
+    timesEaten,
+    hungry
+  };
+}
 
+function greetRider(dragon) {
+  return `Hi, ${dragon.rider}!`;
+}
 
-module.exports = {
+function eat(dragon) {
+  dragon.timesEaten += 1
+  if(dragon.timesEaten < 3){
+    dragon.hungry = true
+  } else {
+    dragon.hungry = false
+  }
+  return dragon
+}
+
+function findFireBreathers(allDragons = []){
+  var fireBreathers = []
+  allDragons.forEach(function(dragon, index){
+    if (dragon.temperment == "aggressive"){
+      fireBreathers.push(dragon)
+    }
+  })
+  return fireBreathers
+}
   // createDragon, 
   // greetRider, 
   // eat, 
   // findFireBreathers
-}
+
+module.exports = {createDragon, greetRider, eat, findFireBreathers};
