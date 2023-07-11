13197ff2e06cecf270040482e15b0c8c392698ee
Ben Rosner <ben.rosner.williamsburg@gmail.com>
2023-07-11 07:54:30 -0400

    Complete dragon fuctionality in mythical creatures

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,8 +1,41 @@
+function createDragon(name, rider, temperment) {
+  var dragon = {
+    name: name,
+    rider: rider,
+    temperment: temperment,
+    timesEaten: 0,
+    hungry: true
+  }
+  return dragon;
+}
 
+function greetRider(dragon) {
+  return `Hi, ${dragon.rider}!`;
+}
+
+function eat(dragon) {
+      if (dragon.timesEaten < 3) {
+        dragon.timesEaten++;
+      }
+      if (dragon.timesEaten >= 3){
+        dragon.hungry = false;
+      }
+  return dragon;
+}
+
+function findFireBreathers(allDragons){
+  var fireBreathers = []
+  for (let i = 0; i < allDragons.length; i++) {
+    if (allDragons[i].temperment === "aggressive"){
+      fireBreathers.push(allDragons[i]);
+    }
+  }
+  return fireBreathers;
+}
 
 module.exports = {
-  // createDragon, 
-  // greetRider, 
-  // eat, 
-  // findFireBreathers
+  createDragon, 
+  greetRider, 
+  eat, 
+  findFireBreathers
 }
