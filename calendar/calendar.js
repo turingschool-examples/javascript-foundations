85d1e184656e91e3c15789e8a5a2765f2515042b
Stephen McPhee <120869196+SMcPhee19@users.noreply.github.com>
2023-12-04 14:42:50 -0700

    finish calendar

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -1,3 +1,36 @@
+function createEvent(title, month, day) {
+  if (day == 0 || day > 31) {
+    return `Error: ${day} is not a valid day`
+  } else {
+    return {
+      title,
+      month,
+      day
+    }
+  }
+}
 
+function createCalendar(person, eventArray) {
+  return {
+    owner: person,
+    events: eventArray
+  }
+}
 
-module.exports = {  };
+function reportMonthlyEvents(calendar, month) {
+  monthlyEvents = []
+  events = calendar.events
+
+  events.forEach(event => {
+    if (event.month == month) {
+      monthlyEvents.push(event)
+    }
+  });
+  return monthlyEvents
+}
+
+module.exports = {
+  createEvent,
+  createCalendar,
+  reportMonthlyEvents
+};
