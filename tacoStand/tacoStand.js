d55f6c0f0036c62522c1436c627b430905f058e8
Cory Sanders <corysanders3@yahoo.com>
2023-12-08 22:48:20 -0700

    add first function to tacostand

--- tacoStand/tacoStand.js
+++ tacoStand/tacoStand.js
@@ -1,2 +1,29 @@
 
-module.exports = {}
+
+function createIngredient(ingredient, price1) {
+  //2 parameters for this function
+  //function needs to take in a name and price
+  //and return an object with a keys of name and price
+  var ingredients = {
+    name: ingredient,
+    price: price1
+  }
+  if (ingredients.name == undefined) {
+    ingredients.name = 'unknown'
+  }
+  if (ingredients.price == undefined) {
+    ingredients.price = 0.00
+  }
+  return ingredients
+};
+
+function createTaco() {
+
+}
+
+
+module.exports = {
+  createIngredient,
+  createTaco
+};
+
