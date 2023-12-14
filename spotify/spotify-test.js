8f86e7fd10e18428f12ed0d4e50ba7428082c00e
Cory Sanders <corysanders3@yahoo.com>
2023-12-14 09:35:03 -0700

    Remove all skips from spotify unit test.

--- spotify/spotify-test.js
+++ spotify/spotify-test.js
@@ -22,11 +22,10 @@ describe('Spotify collections', function() {
         assert.deepEqual(emptyTrack, {title:'unknown',artist:'unknown',  duration:0, });
       })
 
-    it.skip('should only appreciate the talent of Red Hot Chili Peppers', function() {
+    it('should only appreciate the talent of Red Hot Chili Peppers', function() {
         var track1 = createTrack('Californication', 'Red Hot Chili Peppers', 321);
         var track2 = createTrack('Otherside', 'Red Hot Chili Peppers', 255);
         var track3 = createTrack('Beautiful Day', 'U2', 246);
-        
         
         var review1 = reviewTrack(track1);
         var review2 = reviewTrack(track2);
@@ -37,15 +36,16 @@ describe('Spotify collections', function() {
         assert.equal(review3, 'I wish this was a Red Hot Chili Peppers song.'); 
       });
 
-      it.skip('should add one track to a collection', function() {
+      it('should add one track to a collection', function() {
         var chillHits = createCollection('Chill Hits', 'A collection of relaxing songs');
         var track1 = createTrack('Sunset', 'The Midnight', 270);
+
         var chillHitsWith1Track = addTrack(chillHits, track1);
         
         assert.deepEqual(chillHitsWith1Track.tracks, [track1]);
       });
 
-    it.skip('should add tracks to a collection', function() {
+    it('should add tracks to a collection', function() {
         var chillHits = createCollection('Chill Hits', 'A collection of relaxing songs');
         var track1 = createTrack('Sunset', 'The Midnight', 270);
         var track2 = createTrack('Dreaming', 'Small Sails', 215);
@@ -55,16 +55,17 @@ describe('Spotify collections', function() {
         assert.deepEqual(chillHitsWith2Track.tracks, [track1, track2]);
       });
 
-    it.skip('should calculate the total duration of a collection', function() {
+    it('should calculate the total duration of a collection', function() {
         var chillHits = createCollection('Chill Hits', 'A collection of relaxing songs');
         var track1 = createTrack('Sunset', 'The Midnight', 270);
         var track2 = createTrack('Dreaming', 'Small Sails', 215);
         var chillHitsWith1Track = addTrack(chillHits, track1);
         var chillHitsWith2Track = addTrack(chillHitsWith1Track, track2);
+
         assert.equal(getTotalDuration(chillHitsWith2Track), 485);
       });
 
-    it.skip('should find tracks by artist in a collection', function() {
+    it('should find tracks by artist in a collection', function() {
         var chillHits = createCollection('Chill Hits', 'A collection of relaxing songs');
         var track1 = createTrack('Sunset', 'The Midnight', 270);
         var track2 = createTrack('Dreaming', 'Small Sails', 215);
@@ -72,6 +73,7 @@ describe('Spotify collections', function() {
         var chillHitsWith1Track = addTrack(chillHits, track1);
         var chillHitsWith2Track =  addTrack(chillHitsWith1Track, track2);
         var chillHitsWith3Track = addTrack(chillHitsWith2Track, track3);
+
         assert.deepEqual(findTracksByArtist(chillHitsWith3Track, 'The Midnight'), [track1, track3]);
       });
     
--- spotify/spotify.js
+++ spotify/spotify.js
@@ -14,19 +14,69 @@ function createTrack(title1, artist1, duration1) {
     artist: artist1,
     duration: duration1,
   };
-  if (title1 == undefined) {
+  if (!title1) {
       createTrack.title = 'unknown'
   };
-  if (artist1 == undefined) {
+  if (!artist1) {
       createTrack.artist = 'unknown'
   };
-  if (duration1 == undefined) {
+  if (!duration1) {
       createTrack.duration = 0
   };
   return createTrack
 };
 
+function reviewTrack(currentSong) {
+  //1 parameter, takes in an argument of an object
+  //if artist === red hot chili peppers return a postive string
+  //else return a negative string
+  var artist = currentSong.artist;
+  if(artist === 'Red Hot Chili Peppers') {
+    return `The song ${currentSong.title} rules!`
+  } else {
+    return 'I wish this was a Red Hot Chili Peppers song.'
+  };
+};
+
+function addTrack(collection, song) {
+  //2 parameters, first argument an object, with one property being an array.
+  //second argument is an object
+  //push second argument to first argument property array
+  //return the collection once the song is inside the tracks array
+  var tracks = collection.tracks
+  tracks.push(song)
+  return collection
+}
+
+function getTotalDuration(songCollection) {
+  //1 parameter, with an argument of an object, with one property of an array
+  //for loop needed to go through the array
+  //add together the total of the durations
+  var songDuration = 0;
+  for(var i = 0; i < songCollection.tracks.length; i++) {
+    songDuration += songCollection.tracks[i].duration
+  };
+  return songDuration
+};
+
+function findTracksByArtist(collection, artist) {
+  //2 parameters, first argument is an object with a property of an array
+  //second argument is a string
+  //if artist equals The Midnight, push to new array.
+  var midNight = [];
+  for (var i = 0; i < collection.tracks.length; i++) {
+    if(artist === collection.tracks[i].artist) {
+      midNight.push(collection.tracks[i])
+    };
+  };
+  return midNight
+}
+
   module.exports = {
     createCollection,
-    createTrack
+    createTrack,
+    reviewTrack,
+    addTrack,
+    getTotalDuration,
+    findTracksByArtist
   }
